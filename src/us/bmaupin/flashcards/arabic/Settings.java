3b6bbc36bf3ba6e61642ffe8d75a79a13b4082fa
bmaupin <bmaupin@d45c4bbb-d1aa-4eb0-a131-1cfa29d7c7c7>
2011-01-14 19:36:48 +0000

    initial import
    git-svn-id: file:///home/bmaupin/tmp/oldaaf/android_arabic_flashcards/trunk@2 d45c4bbb-d1aa-4eb0-a131-1cfa29d7c7c7

---
+++ src/us/bmaupin/flashcards/arabic/Settings.java
@@ -0,0 +1,20 @@
+package us.bmaupin.flashcards.arabic;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+
+public class Settings extends PreferenceActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.settings);
+	}
+	
+	public static String getDefaultLang(Context context) {
+		return PreferenceManager.getDefaultSharedPreferences(context)
+		       .getString("defaultLang", "arabic");
+	}
+}
