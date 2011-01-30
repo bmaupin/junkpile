2a4210c19d58844d8fbf0bf231b49a67302bdb6e
bmaupin <bmaupin@d45c4bbb-d1aa-4eb0-a131-1cfa29d7c7c7>
2011-01-30 00:57:16 +0000

    revert r31; let's try this again
    git-svn-id: file:///home/bmaupin/tmp/oldaaf/android_arabic_flashcards/trunk@32 d45c4bbb-d1aa-4eb0-a131-1cfa29d7c7c7

---
+++ android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/Settings.java
@@ -0,0 +1,22 @@
+package us.bmaupin.flashcards.arabic;
+
+// $Id$
+
+import android.content.Context;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+
+public class Settings extends PreferenceActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.settings);
+	}
+	
+	public static String getDefaultLang(Context context) {
+		return PreferenceManager.getDefaultSharedPreferences(context)
+		       .getString("defaultLang", "arabic");
+	}
+}
