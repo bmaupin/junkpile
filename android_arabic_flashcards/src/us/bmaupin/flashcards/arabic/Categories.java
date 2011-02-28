75f7643800cefd2c8a257fd105e1a0c0e55e7493
bmaupin <bmaupin@d45c4bbb-d1aa-4eb0-a131-1cfa29d7c7c7>
2011-02-28 20:13:40 +0000

    major cleanup to categories activity
    git-svn-id: file:///home/bmaupin/tmp/oldaaf/android_arabic_flashcards/trunk@89 d45c4bbb-d1aa-4eb0-a131-1cfa29d7c7c7

--- android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/Categories.java
+++ android_arabic_flashcards/src/us/bmaupin/flashcards/arabic/Categories.java
@@ -2,26 +2,20 @@ package us.bmaupin.flashcards.arabic;
 
 // $Id$
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.DialogInterface.OnClickListener;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-//import android.app.ListActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 
 public class Categories extends Activity {
@@ -40,10 +34,8 @@ public class Categories extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.categories);
-		
 
 		int layoutID = android.R.layout.simple_list_item_1;
-		
 		
 		final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
 				this, R.array.categories_main_items,
@@ -51,15 +43,8 @@ public class Categories extends Activity {
 		
 		final ListView lv = (ListView)findViewById(R.id.myListView);
 		
-		lv.setAdapter(adapter);
-		
-		
-//		View v1 = new View(this, null, 0);
-		
-//		lv.addView(child, params)
+		lv.setAdapter(adapter);	
 
-		
-		
 		lv.setOnItemClickListener(new OnItemClickListener() {
 			@Override
 			public void onItemClick(AdapterView<?> parent, View view, int pos,
@@ -69,24 +54,7 @@ public class Categories extends Activity {
 				
 				if (itemText.equals("Ahlan wa sahlan")) {
 					Log.d(TAG, "chose AWS");
-//					chooseAWSChapter();
 					showDialog(DIALOG_AWS_CHAPTER_ID);
-//					DEBUG					
-//					Toast toast = Toast.makeText(context, "selectedChapter: " + selectedChapter, Toast.LENGTH_LONG);
-//					toast.show();
-					/*
-					if (selectedChapter != null) {
-						Intent result = new Intent();
-						result.putExtra(EXTRA_CATEGORY, "Ahlan wa sahlan");
-						result.putExtra(EXTRA_AWS_CHAPTER, selectedChapter);
-						
-						setResult(RESULT_OK, result);
-						finish();
-					} else {
-						setResult(RESULT_CANCELED, null);
-						finish();
-					}
-					*/
 				}
 			}
 			});
@@ -111,10 +79,8 @@ public class Categories extends Activity {
 		builder.setTitle(R.string.choose_aws_chapter_title);
 		builder.setItems(chapters, new DialogInterface.OnClickListener() {
 		    public void onClick(DialogInterface dialog, int item) {
-//		        Toast.makeText(getApplicationContext(), chapters[i], Toast.LENGTH_SHORT).show();
 				Log.d(TAG, "createAWSChapterDialog: int=" + item);
 				Log.d(TAG, "createAWSChapterDialog: chapter=" + chapters[item]);
-//				selectedChapter = chapters[item];
 				
 				Intent result = new Intent();
 				result.putExtra(EXTRA_CATEGORY, "Ahlan wa sahlan");
@@ -128,104 +94,6 @@ public class Categories extends Activity {
 		return ad;
 	}
 	
-	private void chooseAWSChapterOld() {
-		Log.d(TAG, "chooseAWSChapter");
-		
-		AlertDialog.Builder ad = new AlertDialog.Builder(context);
-		ad.setTitle(R.string.choose_aws_chapter_title);
-		
-		final String[] chapters = getChapters();
-		
-		ad.setItems(chapters, new OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialoginterface, int i) {
-						// TODO Auto-generated method stub
-						Log.d(TAG, "chooseAWSChapter: int=" + i);
-						Log.d(TAG, "chooseAWSChapter: chapter=" + chapters[i]);
-						selectedChapter = chapters[i];
-					}
-				});
-
-			ad.show();
-		
-		/*
-		ad.setItems(R.array.aws_chapters, 
-			new OnClickListener() {
-
-				@Override
-				public void onClick(DialogInterface dialoginterface, int i) {
-					// TODO Auto-generated method stub
-					Log.d(TAG, "chooseAWSChapter: int=" + i);
-				}
-			});
-
-		ad.show();
-*/
-
-		
-		
-/*		
-		String title = "It is Pitch Black";
-		String message = "You are likely to be eaten by a grue.";
-		String button1String = "Go Back";
-		String button2String = "Move Forward";
-		AlertDialog.Builder ad = new AlertDialog.Builder(context);
-		ad.setTitle(title);
-		ad.setMessage(message);
-		ad.show();
-		
-/*
-		AlertDialog.Builder ad = new AlertDialog.Builder(context);
-		ad.setTitle(R.string.choose_aws_chapter_title);
-		ad.setMessage(message);
-		ad.setPositiveButton(button1String,
-
-		
-		new AlertDialog.Builder(context)
-			.setTitle(R.string.choose_aws_chapter_title)
-			.setItems(R.array.aws_chapters,
-			new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialoginterface,
-				int i) {
-					Log.d(TAG, "chooseAWSChapter: int=" + i);
-//				Toast toast = Toast.makeText(context, i, Toast.LENGTH_SHORT);
-//				toast.show();
-				}
-			})
-			.show();
-*/
-	}
-
-/*	
-	class ChooseAWSChapter extends Activity {
-
-		public ChooseAWSChapter(Context context) {
-			super(context);
-				
-		}
-
-		@Override
-		protected void onCreate(Bundle savedInstanceState) {
-			super.onCreate(savedInstanceState);
-			
-			setContentView(R.layout.awschapters);
-			
-			int layoutID = android.R.layout.simple_list_item_1;
-			final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-					this, R.array.aws_chapters,
-					layoutID);
-			final ListView tlv = (ListView)findViewById(R.id.myListView);
-			
-			tlv.setAdapter(adapter);
-		}
-		
-	}
-	
-//	class AWSChapters extends Di {
-		
-//	}
-*/
-
 	private String[] getChapters() {
 		Log.d(TAG, "getChapters called");
 		String[] chapters;
