9706ab5b7a328160daab7b9aa3fb012a3da72cbc
bmaupin <bmaupin@d45c4bbb-d1aa-4eb0-a131-1cfa29d7c7c7>
2011-04-22 19:29:44 +0000

    update with latest copy from flashcards app
    git-svn-id: file:///home/bmaupin/tmp/oldaaf/android_arabic_flashcards/trunk@183 d45c4bbb-d1aa-4eb0-a131-1cfa29d7c7c7

--- java/testing/android-test/src/us/bmaupin/test/ProfileDatabaseHelper.java
+++ java/testing/android-test/src/us/bmaupin/test/ProfileDatabaseHelper.java
@@ -1,6 +1,6 @@
 package us.bmaupin.test;
 
-// $Id: ProfileDatabaseHelper.java 173 2011-04-13 18:42:48Z bmaupin $
+//$Id: ProfileDatabaseHelper.java 181 2011-04-22 18:09:34Z bmaupin $
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,103 +14,80 @@ import android.provider.BaseColumns;
 import android.util.Log;
 
 public class ProfileDatabaseHelper extends SQLiteOpenHelper {
-	// tag for log messages
-	public static final String TAG = "ProfileDatabaseHelper";
+    // tag for log messages
+    public static final String TAG = "ProfileDatabaseHelper";
 
     // The name of your database
     public static final String DATABASE_NAME = "profiles.db";
     // The version of your database (increment this every time you change something)
     public static final int DATABASE_VERSION = 1;
-// TODO: we never quite got this working...
     // profile name; this will be used as the database table name
-    private String profileName = "profile1";
-//    private static final String DEFAULT_PROFILE_NAME = "profile1";
-//    public static final String DB_TABLE_NAME = "profile1";
+    private String profileTableName = "profile1";
     
     // The name of each column in the database
-//    public static final String CARD_ID = "card_ID";
     public static final String STATUS = "status";
     
     // SQL Statement to create a new database.
-    private String DB_TABLE_CREATE =
+    private final String DB_TABLE_CREATE =
         "CREATE TABLE %s (" +
         BaseColumns._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
         STATUS + " INTEGER);";
-       
+    
     private final Context context;
-    
-    /*
+
     // The constructor method
-    public ProfileDatabaseHelper(Context context, String DB_TABLE_NAME) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-//        ProfileDatabaseHelper.DB_TABLE_NAME = DB_TABLE_NAME;
-//        this.context = context;
-    }
-    */
-    
     public ProfileDatabaseHelper(Context context) {
-    	super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    	this.context = context;
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.context = context;
     }
     
-	@Override
-	public synchronized SQLiteDatabase getReadableDatabase() {
-	    List<String> tables = new ArrayList<String>();
-	    String SQL_GET_ALL_TABLES = "SELECT name FROM " + 
+    @Override
+    public synchronized SQLiteDatabase getReadableDatabase() {
+        List<String> tables = new ArrayList<String>();
+        final String SQL_GET_ALL_TABLES = "SELECT name FROM " + 
             "sqlite_master WHERE type='table' ORDER BY name";
-	    
-	    // fetch the database
-		SQLiteDatabase db = super.getReadableDatabase();
-		// get the list of tables in the db
-		Cursor cursor = db.rawQuery(SQL_GET_ALL_TABLES, null);
-		cursor.moveToFirst();
-		while (cursor.moveToNext()) {
-		    tables.add(cursor.getString(0));
-		}
-		// if the table we want isn't in the db, create it
-		if (!tables.contains(profileName)) {
-    		// create the profile table
-    		db.execSQL(String.format(DB_TABLE_CREATE, profileName));
-    		// initialize the profile table
-    		initializeDb(db);
-		}
-		
-		return db;
-	}
-	
-	public synchronized SQLiteDatabase getReadableDatabase(String profileName) {
-	    Log.d(TAG, "our getReadableDatabase called");
-	    Log.d(TAG, "our getReadableDatabase: this.profileName=" + profileName);
-	    this.profileName = profileName;
-	    Log.d(TAG, "our getReadableDatabase: this.profileName=" + profileName);
-	    return getReadableDatabase();
-	}
-	
-    /*
-	public String getDB_TABLE_NAME() {
-    	return DB_TABLE_NAME;
+        
+        // fetch the database
+        SQLiteDatabase db = super.getReadableDatabase();
+        // get the list of tables in the db
+        Cursor cursor = db.rawQuery(SQL_GET_ALL_TABLES, null);
+        cursor.moveToFirst();
+        while (cursor.moveToNext()) {
+            tables.add(cursor.getString(0));
+        }
+        cursor.close();
+        // if the table we want isn't in the db, create it
+        if (!tables.contains(profileTableName)) {
+            // create the profile table
+            db.execSQL(String.format(DB_TABLE_CREATE, profileTableName));
+            // initialize the profile table
+            initializeProfileTable(db, profileTableName);
+        }
+        
+        return db;
     }
-    */
-	
+    
+    public synchronized SQLiteDatabase getReadableDatabase(String profileTableName) {
+        Log.d(TAG, "our getReadableDatabase called");
+        Log.d(TAG, "our getReadableDatabase: this.profileTableName=" + profileTableName);
+        this.profileTableName = profileTableName;
+        Log.d(TAG, "our getReadableDatabase: this.profileTableName=" + profileTableName);
+        return getReadableDatabase();
+    }
+    
+    public String getprofileTableName() {
+        return profileTableName;
+    }
+    
     /* Called when the super class getWritableDatabase (or getReadableDatabase)
      * method determines that the database doesn't exist yet and needs to be created
      */
-	/*
     @Override
     public void onCreate(SQLiteDatabase db) {
-    	Log.d(TAG, "onCreate called");
-        db.execSQL(DB_TABLE_CREATE);
-        
-        initializeDb(db);
-    }
-    */
-	
-    @Override
-    public void onCreate(SQLiteDatabase db) {
+//
         Log.d(TAG, "onCreate called");
-        
     }
-
+    
     /* Called when the super class getWritableDatabase (or getReadableDatabase)
      * method determines that the existing database isn't the same version as
      * the latest version
@@ -121,63 +98,52 @@ public class ProfileDatabaseHelper extends SQLiteOpenHelper {
                 + newVersion + ", which will destroy all old data");
 //        db.execSQL("DROP TABLE IF EXISTS " + DB_TABLE_NAME);
 //        onCreate(db);
-//      initializeDb();        
-// TODO: upgrade the db properly
+//TODO: upgrade the db properly
+//        initializeDb(db);
     }
     
+    void initializeProfileTable (SQLiteDatabase db, String profileTable) {
+        String sql = "SELECT COALESCE(MAX(_ID), 0) FROM " + DatabaseHelper.DB_TABLE_NAME;
     
-    void initializeDb (SQLiteDatabase db) {
-    	String sql = "SELECT COALESCE(MAX(_ID), 0) FROM " + DatabaseHelper.DB_TABLE_NAME;
-    	
-    	// get the number of rows in the cards db
-    	DatabaseHelper cardsHelper = new DatabaseHelper(context);
+        // get the number of rows in the cards db
+        DatabaseHelper cardsHelper = new DatabaseHelper(context);
         SQLiteDatabase cardsDb = cardsHelper.getReadableDatabase();
         Cursor cardsCursor = cardsDb.rawQuery(sql, null);
         cardsCursor.moveToFirst();
         int cardsRows = cardsCursor.getInt(0);
         Log.d(TAG, "initializeDb: cardsRows=" + cardsRows );
         cardsCursor.close();
+        cardsDb.close();
         cardsHelper.close();
         
         // get the number of rows in the cards db
-        sql = "SELECT COALESCE(MAX(_ID), 0) FROM " + profileName;
-    	Cursor cursor = db.rawQuery(sql, null);
-    	cursor.moveToFirst();
-    	int profileRows = cursor.getInt(0);
-    	Log.d(TAG, "initializeDb: profileRows=" + profileRows );
-    	
-    	// get the difference, fill the profile db with that number of empty rows
-    	int rowsToAdd = cardsRows - profileRows;
-    	Log.d(TAG, "initializeDb: rowsToAdd=" + rowsToAdd );
-    	Log.d(TAG, "initializeDb: rowsToAdd=" + rowsToAdd );
-    	/*
-    	ContentValues cv=new ContentValues();
-    	for (int i=1; i<rowsToAdd + 1; i++) {
-    		db.insert(DB_TABLE_NAME, RANK, cv);
-    	}
-    	*/
-    	
-    	InsertHelper ih = new InsertHelper(db, profileName);
-    	
-//    	final int ID_COLUMN = ih.getColumnIndex(BaseColumns._ID);
-    	final int STATUS_COLUMN = ih.getColumnIndex(ProfileDatabaseHelper.STATUS);
-    	
-    	
-    	for (int i=1; i<rowsToAdd + 1; i++) {
-    		ih.prepareForInsert();
-//    		ih.bind(ID_COLUMN, RANK_COLUMN);
-    		// just insert an empty row with null values
-    		ih.bind(STATUS_COLUMN, 0);
-    		ih.execute();
-    	}
-    	
-    	cursor = db.rawQuery(sql, null);
-    	cursor.moveToFirst();
-    	profileRows = cursor.getInt(0);
-    	Log.d(TAG, "initializeDb: profileRows=" + profileRows );
-    	cursor.close();
-    	
-// TODO: db fill takes way too long (>5 sec), need to do little by little, or show user progress
-    }
+        sql = "SELECT COALESCE(MAX(_ID), 0) FROM " + profileTable;
+        Cursor cursor = db.rawQuery(sql, null);
+        cursor.moveToFirst();
+        int profileRows = cursor.getInt(0);
+        Log.d(TAG, "initializeDb: profileRows=" + profileRows );
+        
+        // get the difference, fill the profile db with that number of empty rows
+        int rowsToAdd = cardsRows - profileRows;
+        Log.d(TAG, "initializeDb: rowsToAdd=" + rowsToAdd );
+        
+        InsertHelper ih = new InsertHelper(db, profileTable);
+        
+        final int STATUS_COLUMN = ih.getColumnIndex(ProfileDatabaseHelper.STATUS);
+        
+        for (int i=1; i<rowsToAdd + 1; i++) {
+            ih.prepareForInsert();
+            // insert a row with a status of 0 (unseen)
+//            ih.bind(STATUS_COLUMN, 0);
+            ih.execute();
+        }
+//
+        cursor = db.rawQuery(sql, null);
+        cursor.moveToFirst();
+        profileRows = cursor.getInt(0);
+        Log.d(TAG, "initializeDb: profileRows=" + profileRows );
+        cursor.close();
     
+    //TODO: db fill takes way too long (>5 sec), need to do little by little, or show user progress
+    }
 }
