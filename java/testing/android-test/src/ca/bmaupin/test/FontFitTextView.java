47866bfb0a64194204c0d544fe07b4b1716d2dc0
b <bmaupincode@gmail.com>
2012-06-15 12:25:41 -0500

    start working on getting view to resize on words and not entire text.  a big bug remains where the view draws before the resizing is finished

--- java/testing/android-test/src/ca/bmaupin/test/FontFitTextView.java
+++ java/testing/android-test/src/ca/bmaupin/test/FontFitTextView.java
@@ -26,15 +26,60 @@ public class FontFitTextView extends TextView {
             return;
         }
         
+        float newSize = 0;
         Paint paint = this.getPaint();
         int maxWidth = viewWidth - this.getPaddingLeft() - 
                 this.getPaddingRight();
-
-//        
+        
+//      
         Log.d(TAG, "viewWidth=" + viewWidth);
         Log.d(TAG, "maxWidth=" + maxWidth);
         Log.d(TAG, "this.getTextSize()=" + this.getTextSize());
         Log.d(TAG, "paint.measureText(text)=" + paint.measureText(text));
+        
+        String[] words = text.split(" ");
+        
+        for (String word : words) {
+            // if this word isn't larger than the max size
+            if (paint.measureText(word) < maxWidth) {
+                // go to the next
+                continue;
+            }
+            
+            Log.d(TAG, "word=" + word);
+
+            float hi = paint.measureText(word);
+            float lo = 2;
+            final float threshold = 0.5f; // How close we have to be
+
+            while ((hi - lo) > threshold) {
+                float size = (hi + lo) / 2;
+                paint.setTextSize(size);
+                if (paint.measureText(word) >= maxWidth) 
+                    hi = size; // too big
+                else
+                    lo = size; // too small
+            }
+            
+            Log.d(TAG, "lo=" + lo);
+            
+            // only update newSize the first time or if the new value is smaller
+            if (newSize == 0 || lo < newSize) {
+                // Use lo so that we undershoot rather than overshoot
+                newSize = lo;
+            }
+            Log.d(TAG, "newSize=" + newSize);
+        }
+        
+        
+        if (newSize != 0) {
+            this.setTextSize(TypedValue.COMPLEX_UNIT_PX, newSize);
+            Log.d(TAG, "newSize=" + newSize);
+        }
+        
+        Log.d(TAG, "this.getTextSize())" + this.getTextSize());
+/*
+
         
         // no need to shrink the text if it isn't larger than the max size
         if (paint.measureText(text) < maxWidth) {
@@ -53,10 +98,10 @@ public class FontFitTextView extends TextView {
             else
                 lo = size; // too small
         }
-        // Use lo so that we undershoot rather than overshoot
-        this.setTextSize(TypedValue.COMPLEX_UNIT_PX, lo);
+*/
+
     }
-/*
+
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
@@ -65,7 +110,7 @@ public class FontFitTextView extends TextView {
         refitText(this.getText().toString(), parentWidth);
         this.setMeasuredDimension(parentWidth, height);
     }
-*/
+
     @Override
     protected void onTextChanged(final CharSequence text, final int start, 
             final int lengthBefore, final int lengthAfter) {
