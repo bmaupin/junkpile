0a8e1b87158e4f303f67cedd1f2cf363d9a19c6c
b <bmaupin@users.noreply.github.com>
2012-06-20 13:42:40 -0500

    do some cleanup and rename the class.  however it doesn't appear to work in landscape mode on ICS

--- java/testing/android-test/src/ca/bmaupin/test/AutofitTextView.java
+++ java/testing/android-test/src/ca/bmaupin/test/AutofitTextView.java
@@ -7,14 +7,14 @@ import android.util.Log;
 import android.util.TypedValue;
 import android.widget.TextView;
 
-public class FontFitTextView extends TextView {
+public class AutofitTextView extends TextView {
     private String TAG = "FontFitTextView";
     
-    public FontFitTextView(Context context) {
+    public AutofitTextView(Context context) {
         super(context);
     }
 
-    public FontFitTextView(Context context, AttributeSet attrs) {
+    public AutofitTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
@@ -31,12 +31,6 @@ public class FontFitTextView extends TextView {
         int maxWidth = viewWidth - this.getPaddingLeft() - 
                 this.getPaddingRight();
         
-//      
-        Log.d(TAG, "viewWidth=" + viewWidth);
-        Log.d(TAG, "maxWidth=" + maxWidth);
-        Log.d(TAG, "this.getTextSize()=" + this.getTextSize());
-        Log.d(TAG, "paint.measureText(text)=" + paint.measureText(text));
-        
         String[] words = text.split(" ");
         
         for (String word : words) {
@@ -45,8 +39,6 @@ public class FontFitTextView extends TextView {
                 // go to the next
                 continue;
             }
-            
-            Log.d(TAG, "word=" + word);
 
             float hi = paint.measureText(word);
             float lo = 2;
@@ -61,14 +53,11 @@ public class FontFitTextView extends TextView {
                     lo = size; // too small
             }
             
-            Log.d(TAG, "lo=" + lo);
-            
             // only update newSize the first time or if the new value is smaller
             if (newSize == 0 || lo < newSize) {
                 // Use lo so that we undershoot rather than overshoot
                 newSize = lo;
             }
-            Log.d(TAG, "newSize=" + newSize);
         }
         
         
@@ -77,43 +66,20 @@ public class FontFitTextView extends TextView {
             // recalculate the line wrapping (pre ICS; ICS doesn't seem to need
             // this
             setEllipsize(null);
-            Log.d(TAG, "newSize=" + newSize);
         }
-        
-        Log.d(TAG, "this.getTextSize())" + this.getTextSize());
-/*
-
-        
-        // no need to shrink the text if it isn't larger than the max size
-        if (paint.measureText(text) < maxWidth) {
-            return;
-        }
-
-        float hi = paint.measureText(text);
-        float lo = 2;
-        final float threshold = 0.5f; // How close we have to be
-
-        while ((hi - lo) > threshold) {
-            float size = (hi + lo) / 2;
-            paint.setTextSize(size);
-            if (paint.measureText(text) >= maxWidth) 
-                hi = size; // too big
-            else
-                lo = size; // too small
-        }
-*/
-
     }
 
+// TODO not sure how necessary this is...
+/*
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         Log.d(TAG, "onMeasure()");
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         int parentWidth = MeasureSpec.getSize(widthMeasureSpec);
         int height = getMeasuredHeight();
-        refitText(this.getText().toString(), parentWidth);
         this.setMeasuredDimension(parentWidth, height);
     }
+*/
 
     @Override
     protected void onTextChanged(final CharSequence text, final int start, 
