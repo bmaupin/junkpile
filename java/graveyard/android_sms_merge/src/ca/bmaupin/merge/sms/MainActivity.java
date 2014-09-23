07d5247bb143140eaf99be89915ff999de356275
bmaupin <bmaupincode@gmail.com>
2014-09-23 13:55:20 -0400

    Commit current progress

--- java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/MainActivity.java
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/MainActivity.java
@@ -1,24 +1,38 @@
 package ca.bmaupin.merge.sms;
 
+import ca.bmaupin.merge.sms.data.Conversation;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.content.AsyncQueryHandler;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.os.Build;
+import android.provider.Telephony.Threads;
 
 
 
 public class MainActivity extends ActionBarActivity {
+	private static final String TAG = "ConversationList";
+	
+    private static final int THREAD_LIST_QUERY_TOKEN       = 1701;
+    
+    private AsyncQueryHandler mQueryHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        //setContentView(R.layout.conversation_list_screen);
+        
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
@@ -57,8 +71,26 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            //View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            
+            View rootView = inflater.inflate(R.layout.conversation_list_screen, container, false);
+            
+            TextView tv = (TextView) rootView.findViewById(R.id.test_textview);
+            
+            tv.setText("Hey, it worked!");
+            
             return rootView;
+        }
+    }
+    
+    private void startAsyncQuery() {
+        try {
+//            ((TextView)(getListView().getEmptyView())).setText(R.string.loading_conversations);
+
+            Conversation.startQueryForAll(mQueryHandler, THREAD_LIST_QUERY_TOKEN);
+//            Conversation.startQuery(mQueryHandler, UNREAD_THREADS_QUERY_TOKEN, Threads.READ + "=0");
+        } catch (SQLiteException e) {
+        	Log.e(TAG, "SQLiteException: " + e);
         }
     }
 }
