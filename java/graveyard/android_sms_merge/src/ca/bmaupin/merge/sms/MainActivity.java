26c151f2ec81e3ef11b85e14b2d59c607faaa78d
bmaupin <bmaupin@users.noreply.github.com>
2014-09-25 14:08:56 -0400

    Start adding stuff so we can read SMS messages

--- java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/MainActivity.java
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/MainActivity.java
@@ -2,7 +2,13 @@ package ca.bmaupin.merge.sms;
 
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Telephony.Threads;
+import android.provider.Telephony.Sms.Conversations;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,7 +18,16 @@ import android.widget.TextView;
 
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements LoaderCallbacks<Cursor> {
+	public static final Uri sAllThreadsUri =
+	        Threads.CONTENT_URI.buildUpon().appendQueryParameter("simple", "true").build();
+	
+    public static final String[] ALL_THREADS_PROJECTION = {
+        Threads._ID, Threads.DATE, Threads.MESSAGE_COUNT, Threads.RECIPIENT_IDS,
+        Threads.SNIPPET, Threads.SNIPPET_CHARSET, Threads.READ, Threads.ERROR,
+        Threads.HAS_ATTACHMENT
+    };
+	
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,9 +80,48 @@ public class MainActivity extends ActionBarActivity {
 			// TODO Auto-generated method stub
 			super.onActivityCreated(savedInstanceState);
 
+			Cursor mCursor = getActivity().getContentResolver().query(
+					sAllThreadsUri,
+					ALL_THREADS_PROJECTION,
+					null, // selection
+					null, // selectionArgs
+					Conversations.DEFAULT_SORT_ORDER
+					);
+					
+					
+					/*
+				    UserDictionary.Words.CONTENT_URI,   // The content URI of the words table
+				    mProjection,                        // The columns to return for each row
+				    mSelectionClause                    // Selection criteria
+				    mSelectionArgs,                     // Selection criteria
+				    mSortOrder);
+				    */ 
+			
 // TEST			
 			TextView tv = (TextView) getActivity().findViewById(R.id.test_textview);
 			tv.setText("Howdy");
+			
+			
 		}
     }
+
+	@Override
+	public Loader<Cursor> onCreateLoader(int arg0, Bundle arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> arg0, Cursor arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void onLoaderReset(Loader<Cursor> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 }
