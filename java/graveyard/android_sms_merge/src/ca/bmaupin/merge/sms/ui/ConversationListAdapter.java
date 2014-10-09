a00fb88951a2aab0b79660731b111323d0b8ca56
bmaupin <bmaupincode@gmail.com>
2014-10-09 12:59:18 -0400

    Add ConversationListAdapter with minor modifications for now

---
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/ui/ConversationListAdapter.java
@@ -0,0 +1,57 @@
+/*
+ * Derived from com.android.mms/src/com/android/mms/ui/ConversationListAdapter.java
+ */
+
+package ca.bmaupin.merge.sms.ui;
+
+import ca.bmaupin.merge.sms.R;
+import ca.bmaupin.merge.sms.R.layout;
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
+
+/**
+ * The back-end data adapter for ConversationList.
+ */
+//TODO: This should be public class ConversationListAdapter extends ArrayAdapter<Conversation>
+public class ConversationListAdapter extends CursorAdapter implements AbsListView.RecyclerListener {
+    private static final String TAG = "ConversationListAdapter";
+    private static final boolean LOCAL_LOGV = false;
+
+    private final LayoutInflater mFactory;
+
+    public ConversationListAdapter(Context context, Cursor cursor) {
+        super(context, cursor, false /* auto-requery */);
+        mFactory = LayoutInflater.from(context);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        if (!(view instanceof ConversationListItem)) {
+            Log.e(TAG, "Unexpected bound view: " + view);
+            return;
+        }
+
+        ConversationListItem headerView = (ConversationListItem) view;
+        Conversation conv = Conversation.from(context, cursor);
+        headerView.bind(context, conv);
+    }
+
+    public void onMovedToScrapHeap(View view) {
+        ConversationListItem headerView = (ConversationListItem)view;
+        headerView.unbind();
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        if (LOCAL_LOGV) Log.v(TAG, "inflating new view");
+        return mFactory.inflate(R.layout.conversation_list_item, parent, false);
+    }
+}
