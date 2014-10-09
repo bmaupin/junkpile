2bc9919d35024997fed5b46e71a20dc5ba1c2555
bmaupin <bmaupincode@gmail.com>
2014-10-09 13:14:04 -0400

    Fix package and imports

--- java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/ui/ConversationListItem.java
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/ui/ConversationListItem.java
@@ -1,21 +1,9 @@
 /*
- * Copyright (C) 2008 Esmertec AG.
- * Copyright (C) 2008 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Derived from com.android.mms/src/com/android/mms/ui/ConversationListItem.java
+ * (tag android-4.4_r1.2.0.1)
  */
 
-package com.android.mms.ui;
+package ca.bmaupin.merge.sms.ui;
 
 import android.content.Context;
 import android.graphics.Typeface;
@@ -34,11 +22,7 @@ import android.widget.QuickContactBadge;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-import com.android.mms.LogTag;
-import com.android.mms.R;
-import com.android.mms.data.Contact;
-import com.android.mms.data.ContactList;
-import com.android.mms.data.Conversation;
+import ca.bmaupin.merge.sms.R;
 
 /**
  * This class manages the view for given conversation.
