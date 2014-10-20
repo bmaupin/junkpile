30917297b32e7fc69527f2bf36afde38985cba4b
bmaupin <bmaupin@users.noreply.github.com>
2014-10-20 13:34:35 -0400

    Basic modifications to get it working

--- java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/LogTag.java
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/LogTag.java
@@ -1,17 +1,6 @@
 /*
- * Copyright (C) 2009 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Derived from com.android.mms/src/com/android/mms/LogTag.java
+ * (tag android-4.4_r1.2.0.1)
  */
 
 package ca.bmaupin.merge.sms;
@@ -23,9 +12,9 @@ import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.util.Log;
 
-import com.android.mms.data.Contact;
-import com.android.mms.data.Conversation;
-import com.android.mms.data.RecipientIdCache;
+import ca.bmaupin.merge.sms.data.Contact;
+import ca.bmaupin.merge.sms.data.Conversation;
+import ca.bmaupin.merge.sms.data.RecipientIdCache;
 
 public class LogTag {
     public static final String TAG = "Mms";
