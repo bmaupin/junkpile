74907ad75a6dc9e9865f94f1a898ee1b93600600
bmaupin <bmaupin@users.noreply.github.com>
2014-10-20 14:24:50 -0400

    Basic fixes

--- java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/util/SqliteWrapper.java
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/util/SqliteWrapper.java
@@ -1,21 +1,9 @@
 /*
- * Copyright (C) 2008 Esmertec AG.
- * Copyright (C) 2008 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Derived from android.database.sqlite.SqliteWrapper.java
+ * (tag android-4.4.4_r2.0.1)
  */
 
-package android.database.sqlite;
+package ca.bmaupin.merge.sms.util;
 
 import android.content.ContentResolver;
 import android.content.ContentValues;
@@ -25,6 +13,8 @@ import android.database.sqlite.SQLiteException;
 import android.net.Uri;
 import android.util.Log;
 import android.widget.Toast;
+
+import ca.bmaupin.merge.sms.R;
 
 /**
  * @hide
@@ -46,7 +36,7 @@ public final class SqliteWrapper {
 
     public static void checkSQLiteException(Context context, SQLiteException e) {
         if (isLowMemory(e)) {
-            Toast.makeText(context, com.android.internal.R.string.low_memory,
+            Toast.makeText(context, R.string.low_memory,
                     Toast.LENGTH_SHORT).show();
         } else {
             throw e;
