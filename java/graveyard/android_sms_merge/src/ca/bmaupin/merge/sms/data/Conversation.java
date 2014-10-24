3defc9c85b03550caf4bff773c06c2d61184c98c
bmaupin <bmaupin@users.noreply.github.com>
2014-10-24 13:49:14 -0400

    Add Conversation with pristine from() method

---
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/data/Conversation.java
@@ -0,0 +1,45 @@
+/*
+ * Derived from com.android.mms.data.Conversation
+ * (tag android-4.4.4_r2.0.1)
+ */
+
+package ca.bmaupin.merge.sms.data;
+
+import android.content.Context;
+import android.database.Cursor;
+
+public class Conversation {
+	private static final int ID             = 0;
+	
+    /**
+     * Returns a temporary Conversation (not representing one on disk) wrapping
+     * the contents of the provided cursor.  The cursor should be the one
+     * returned to your AsyncQueryHandler passed in to {@link #startQueryForAll}.
+     * The recipient list of this conversation can be empty if the results
+     * were not in cache.
+     */
+    public static Conversation from(Context context, Cursor cursor) {
+        // First look in the cache for the Conversation and return that one. That way, all the
+        // people that are looking at the cached copy will get updated when fillFromCursor() is
+        // called with this cursor.
+        long threadId = cursor.getLong(ID);
+        if (threadId > 0) {
+            Conversation conv = Cache.get(threadId);
+            if (conv != null) {
+                fillFromCursor(context, conv, cursor, false);   // update the existing conv in-place
+                return conv;
+            }
+        }
+        Conversation conv = new Conversation(context, cursor, false);
+        try {
+            Cache.put(conv);
+        } catch (IllegalStateException e) {
+            LogTag.error(TAG, "Tried to add duplicate Conversation to Cache (from cursor): " +
+                    conv);
+            if (!Cache.replace(conv)) {
+                LogTag.error("Converations.from cache.replace failed on " + conv);
+            }
+        }
+        return conv;
+    }
+}
