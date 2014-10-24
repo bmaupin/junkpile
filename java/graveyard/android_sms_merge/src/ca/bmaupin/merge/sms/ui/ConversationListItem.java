1279a9c1bcd5a7c142eb534f0f66aee19b136301
bmaupin <bmaupin@users.noreply.github.com>
2014-10-24 14:15:32 -0400

    Ridiculous, unfortunately, but after all I don't think errors, unread, and drafts would make much sense in the context of the app

--- java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/ui/ConversationListItem.java
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/ui/ConversationListItem.java
@@ -37,7 +37,6 @@ public class ConversationListItem extends RelativeLayout {
     private TextView mFromView;
     private TextView mDateView;
     private View mAttachmentView;
-    private View mErrorIndicator;
     private QuickContactBadge mAvatarView;
 
     static private Drawable sDefaultContactImage;
@@ -72,7 +71,6 @@ public class ConversationListItem extends RelativeLayout {
 
         mDateView = (TextView) findViewById(R.id.date);
         mAttachmentView = findViewById(R.id.attachment);
-        mErrorIndicator = findViewById(R.id.error);
         mAvatarView = (QuickContactBadge) findViewById(R.id.avatar);
     }
 
@@ -106,24 +104,7 @@ public class ConversationListItem extends RelativeLayout {
                     mContext.getResources().getColor(R.color.message_count_color)),
                     before, buf.length(), Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
         }
-        if (mConversation.hasDraft()) {
-            buf.append(mContext.getResources().getString(R.string.draft_separator));
-            int before = buf.length();
-            int size;
-            buf.append(mContext.getResources().getString(R.string.has_draft));
-            size = android.R.style.TextAppearance_Small;
-            buf.setSpan(new TextAppearanceSpan(mContext, size, color), before,
-                    buf.length(), Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
-            buf.setSpan(new ForegroundColorSpan(
-                    mContext.getResources().getColor(R.drawable.text_color_red)),
-                    before, buf.length(), Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
-        }
 
-        // Unread messages are shown in bold
-        if (mConversation.hasUnreadMessages()) {
-            buf.setSpan(STYLE_BOLD, 0, buf.length(),
-                    Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
-        }
         return buf;
     }
 
@@ -157,19 +138,6 @@ public class ConversationListItem extends RelativeLayout {
 
         mConversation = conversation;
 
-        updateBackground();
-
-        LayoutParams attachmentLayout = (LayoutParams)mAttachmentView.getLayoutParams();
-        boolean hasError = conversation.hasError();
-        // When there's an error icon, the attachment icon is left of the error icon.
-        // When there is not an error icon, the attachment icon is left of the date text.
-        // As far as I know, there's no way to specify that relationship in xml.
-        if (hasError) {
-            attachmentLayout.addRule(RelativeLayout.LEFT_OF, R.id.error);
-        } else {
-            attachmentLayout.addRule(RelativeLayout.LEFT_OF, R.id.date);
-        }
-
         boolean hasAttachment = conversation.hasAttachment();
         mAttachmentView.setVisibility(hasAttachment ? VISIBLE : GONE);
 
@@ -184,23 +152,9 @@ public class ConversationListItem extends RelativeLayout {
         LayoutParams subjectLayout = (LayoutParams)mSubjectView.getLayoutParams();
         // We have to make the subject left of whatever optional items are shown on the right.
         subjectLayout.addRule(RelativeLayout.LEFT_OF, hasAttachment ? R.id.attachment :
-            (hasError ? R.id.error : R.id.date));
-
-        // Transmission error indicator.
-        mErrorIndicator.setVisibility(hasError ? VISIBLE : GONE);
+            (R.id.date));
 
         updateAvatarView();
-    }
-
-    private void updateBackground() {
-        int backgroundId;
-        if (mConversation.hasUnreadMessages()) {
-            backgroundId = R.drawable.conversation_item_background_unread;
-        } else {
-            backgroundId = R.drawable.conversation_item_background_read;
-        }
-        Drawable background = mContext.getResources().getDrawable(backgroundId);
-        setBackground(background);
     }
 
     public final void unbind() {
