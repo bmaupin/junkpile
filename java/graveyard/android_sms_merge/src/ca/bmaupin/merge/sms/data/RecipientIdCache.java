3ade1d21d7fad27ab303740fdb91a2ae63f83367
bmaupin <bmaupincode@gmail.com>
2014-10-27 13:54:00 -0400

    Add empty RecipientIdCache

---
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/data/RecipientIdCache.java
@@ -0,0 +1,12 @@
+/*
+ * Derived from com.android.mms.data.RecipientIdCache
+ * (tag android-4.4.4_r2.0.1)
+ */
+
+package ca.bmaupin.merge.sms.data;
+
+import javax.annotation.concurrent.ThreadSafe;
+
+@ThreadSafe
+public class RecipientIdCache {
+}
