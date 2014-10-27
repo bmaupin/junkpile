17ac175b65de931087615a89b16ff93c23a25abd
bmaupin <bmaupin@users.noreply.github.com>
2014-10-27 13:27:10 -0400

    Add empty ContactList

---
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/data/ContactList.java
@@ -0,0 +1,12 @@
+/*
+ * Derived from com.android.mms.data.ContactList
+ * (tag android-4.4.4_r2.0.1)
+ */
+
+package ca.bmaupin.merge.sms.data;
+
+import java.util.ArrayList;
+
+public class ContactList extends ArrayList<Contact> {
+
+}
