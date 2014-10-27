a84389ddacf5c22874eca90b8b5fd9f251f80b7a
bmaupin <bmaupincode@gmail.com>
2014-10-27 13:15:28 -0400

    Add a barebones LogTag for now

---
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/LogTag.java
@@ -0,0 +1,10 @@
+/*
+ * Derived from com.android.mms.LogTag
+ * (tag android-4.4.4_r2.0.1)
+ */
+
+package ca.bmaupin.merge.sms;
+
+public class LogTag {
+    public static final String THREAD_CACHE = "Mms:threadcache";
+}
