d03a595575b8af8f4c936cee71d15140ea4e9653
bmaupin <bmaupin@users.noreply.github.com>
2014-10-29 14:41:37 -0400

    Commit progress on Contact :P

--- java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/data/Contact.java
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/data/Contact.java
@@ -5,6 +5,12 @@
 
 package ca.bmaupin.merge.sms.data;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+import com.android.mms.data.Contact;
+
 import android.content.ContentUris;
 import android.content.Context;
 import android.graphics.Bitmap;
@@ -13,18 +19,125 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.provider.ContactsContract.Contacts;
+import android.telephony.PhoneNumberUtils;
 import android.text.TextUtils;
+import android.util.Log;
+import ca.bmaupin.merge.sms.LogTag;
 
 public class Contact {
+	private static final String TAG = "Contact";
+	private static ContactsCache sContactCache;
+	
+	private final static HashSet<UpdateListener> mListeners = new HashSet<UpdateListener>();
+	
+    private long mContactMethodId;   // Id in phone or email Uri returned by provider of current
+                                     // Contact, -1 is invalid. e.g. contact method id is 20 when
+                                     // current contact has phone content://.../phones/20.
+    private int mContactMethodType;
     private String mNumber;
+    private String mNumberE164;
     private String mName;
+    private String mNameAndNumber;   // for display, e.g. Fred Flintstone <670-782-1123>
     
+    private long mRecipientId;       // used to find the Recipient cache entry
+    private String mLabel;
     private long mPersonId;
+    private int mPresenceResId;      // TODO: make this a state instead of a res ID
+    private String mPresenceText;
     private BitmapDrawable mAvatar;
     private byte [] mAvatarData;
+    private boolean mIsStale;
+    private boolean mQueryPending;
+    private boolean mSendToVoicemail;   // true if this contact should not put up notification
+    
+    public interface UpdateListener {
+        public void onUpdate(Contact updated);
+    }
 	
+    public static void logWithTrace(String tag, String msg, Object... format) {
+        Thread current = Thread.currentThread();
+        StackTraceElement[] stack = current.getStackTrace();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        sb.append(current.getId());
+        sb.append("] ");
+        sb.append(String.format(msg, format));
+
+        sb.append(" <- ");
+        int stop = stack.length > 7 ? 7 : stack.length;
+        for (int i = 3; i < stop; i++) {
+            String methodName = stack[i].getMethodName();
+            sb.append(methodName);
+            if ((i+1) != stop) {
+                sb.append(" <- ");
+            }
+        }
+
+        Log.d(tag, sb.toString());
+    }
+    
+    public static Contact get(String number, boolean canBlock) {
+        return sContactCache.get(number, canBlock);
+    }
+    
     public synchronized boolean existsInDatabase() {
         return (mPersonId > 0);
+    }
+    
+    private static String emptyIfNull(String s) {
+        return (s != null ? s : "");
+    }
+    
+    /**
+     * Fomat the name and number.
+     *
+     * @param name
+     * @param number
+     * @param numberE164 the number's E.164 representation, is used to get the
+     *        country the number belongs to.
+     * @return the formatted name and number
+     */
+    public static String formatNameAndNumber(String name, String number, String numberE164) {
+        // Format like this: Mike Cleron <(650) 555-1234>
+        //                   Erick Tseng <(650) 555-1212>
+        //                   Tutankhamun <tutank1341@gmail.com>
+        //                   (408) 555-1289
+        String formattedNumber = number;
+        if (!Mms.isEmailAddress(number)) {
+            formattedNumber = PhoneNumberUtils.formatNumber(number, numberE164,
+                    MmsApp.getApplication().getCurrentCountryIso());
+        }
+
+        if (!TextUtils.isEmpty(name) && !name.equals(number)) {
+            return name + " <" + formattedNumber + ">";
+        } else {
+            return formattedNumber;
+        }
+    }
+    
+    public synchronized String getNumber() {
+        return mNumber;
+    }
+    
+    public synchronized String getName() {
+        if (TextUtils.isEmpty(mName)) {
+            return mNumber;
+        } else {
+            return mName;
+        }
+    }
+    
+    private void notSynchronizedUpdateNameAndNumber() {
+        mNameAndNumber = formatNameAndNumber(mName, mNumber, mNumberE164);
+    }
+    
+    public synchronized void setRecipientId(long id) {
+        mRecipientId = id;
+    }
+    
+    public synchronized Uri getUri() {
+        return ContentUris.withAppendedId(Contacts.CONTENT_URI, mPersonId);
     }
     
     public synchronized Drawable getAvatar(Context context, Drawable defaultValue) {
@@ -37,19 +150,300 @@ public class Contact {
         return mAvatar != null ? mAvatar : defaultValue;
     }
     
-    public synchronized String getName() {
-        if (TextUtils.isEmpty(mName)) {
-            return mNumber;
-        } else {
-            return mName;
+    private static class ContactsCache {
+    	private final TaskStack mTaskQueue = new TaskStack();
+    	
+        private static class TaskStack {
+            Thread mWorkerThread;
+            private final ArrayList<Runnable> mThingsToLoad;
+
+            public TaskStack() {
+                mThingsToLoad = new ArrayList<Runnable>();
+                mWorkerThread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while (true) {
+                            Runnable r = null;
+                            synchronized (mThingsToLoad) {
+                                if (mThingsToLoad.size() == 0) {
+                                    try {
+                                        mThingsToLoad.wait();
+                                    } catch (InterruptedException ex) {
+                                        break;  // Exception sent by Contact.init() to stop Runnable
+                                    }
+                                }
+                                if (mThingsToLoad.size() > 0) {
+                                    r = mThingsToLoad.remove(0);
+                                }
+                            }
+                            if (r != null) {
+                                r.run();
+                            }
+                        }
+                    }
+                }, "Contact.ContactsCache.TaskStack worker thread");
+                mWorkerThread.setPriority(Thread.MIN_PRIORITY);
+                mWorkerThread.start();
+            }
+
+            public void push(Runnable r) {
+                synchronized (mThingsToLoad) {
+                    mThingsToLoad.add(r);
+                    mThingsToLoad.notify();
+                }
+            }
+        }
+    	
+        public void pushTask(Runnable r) {
+            mTaskQueue.push(r);
+        }
+    	
+        public Contact get(String number, boolean canBlock) {
+            return get(number, false, canBlock);
+        }
+
+        private Contact get(String number, boolean isMe, boolean canBlock) {
+            if (Log.isLoggable(LogTag.CONTACT, Log.DEBUG)) {
+                logWithTrace(TAG, "get(%s, %s, %s)", number, isMe, canBlock);
+            }
+
+            if (TextUtils.isEmpty(number)) {
+                number = "";        // In some places (such as Korea), it's possible to receive
+                                    // a message without the sender's address. In this case,
+                                    // all such anonymous messages will get added to the same
+                                    // thread.
+            }
+
+            // Always return a Contact object, if if we don't have an actual contact
+            // in the contacts db.
+            Contact contact = internalGet(number, isMe);
+            Runnable r = null;
+
+            synchronized (contact) {
+                // If there's a query pending and we're willing to block then
+                // wait here until the query completes.
+                while (canBlock && contact.mQueryPending) {
+                    try {
+                        contact.wait();
+                    } catch (InterruptedException ex) {
+                        // try again by virtue of the loop unless mQueryPending is false
+                    }
+                }
+
+                // If we're stale and we haven't already kicked off a query then kick
+                // it off here.
+                if (contact.mIsStale && !contact.mQueryPending) {
+                    contact.mIsStale = false;
+
+                    if (Log.isLoggable(LogTag.APP, Log.VERBOSE)) {
+                        log("async update for " + contact.toString() + " canBlock: " + canBlock +
+                                " isStale: " + contact.mIsStale);
+                    }
+
+                    final Contact c = contact;
+                    r = new Runnable() {
+                        @Override
+                        public void run() {
+                            updateContact(c);
+                        }
+                    };
+
+                    // set this to true while we have the lock on contact since we will
+                    // either run the query directly (canBlock case) or push the query
+                    // onto the queue.  In either case the mQueryPending will get set
+                    // to false via updateContact.
+                    contact.mQueryPending = true;
+                }
+            }
+            // do this outside of the synchronized so we don't hold up any
+            // subsequent calls to "get" on other threads
+            if (r != null) {
+                if (canBlock) {
+                    r.run();
+                } else {
+                    pushTask(r);
+                }
+            }
+            return contact;
+        }
+        
+        private boolean contactChanged(Contact orig, Contact newContactData) {
+            // The phone number should never change, so don't bother checking.
+            // TODO: Maybe update it if it has gotten longer, i.e. 650-234-5678 -> +16502345678?
+
+            // Do the quick check first.
+            if (orig.mContactMethodType != newContactData.mContactMethodType) {
+                return true;
+            }
+
+            if (orig.mContactMethodId != newContactData.mContactMethodId) {
+                return true;
+            }
+
+            if (orig.mPersonId != newContactData.mPersonId) {
+                if (Log.isLoggable(LogTag.CONTACT, Log.DEBUG)) {
+                    Log.d(TAG, "person id changed");
+                }
+                return true;
+            }
+
+            if (orig.mPresenceResId != newContactData.mPresenceResId) {
+                if (Log.isLoggable(LogTag.CONTACT, Log.DEBUG)) {
+                    Log.d(TAG, "presence changed");
+                }
+                return true;
+            }
+
+            if (orig.mSendToVoicemail != newContactData.mSendToVoicemail) {
+                return true;
+            }
+
+            String oldName = emptyIfNull(orig.mName);
+            String newName = emptyIfNull(newContactData.mName);
+            if (!oldName.equals(newName)) {
+                if (Log.isLoggable(LogTag.CONTACT, Log.DEBUG)) {
+                    Log.d(TAG, String.format("name changed: %s -> %s", oldName, newName));
+                }
+                return true;
+            }
+
+            String oldLabel = emptyIfNull(orig.mLabel);
+            String newLabel = emptyIfNull(newContactData.mLabel);
+            if (!oldLabel.equals(newLabel)) {
+                if (Log.isLoggable(LogTag.CONTACT, Log.DEBUG)) {
+                    Log.d(TAG, String.format("label changed: %s -> %s", oldLabel, newLabel));
+                }
+                return true;
+            }
+
+            if (!Arrays.equals(orig.mAvatarData, newContactData.mAvatarData)) {
+                if (Log.isLoggable(LogTag.CONTACT, Log.DEBUG)) {
+                    Log.d(TAG, "avatar changed");
+                }
+                return true;
+            }
+
+            return false;
+        }
+        
+        private void updateContact(final Contact c) {
+            if (c == null) {
+                return;
+            }
+
+            Contact entry = getContactInfo(c);
+            synchronized (c) {
+                if (contactChanged(c, entry)) {
+                    if (Log.isLoggable(LogTag.APP, Log.VERBOSE)) {
+                        log("updateContact: contact changed for " + entry.mName);
+                    }
+
+                    c.mNumber = entry.mNumber;
+                    c.mLabel = entry.mLabel;
+                    c.mPersonId = entry.mPersonId;
+                    c.mPresenceResId = entry.mPresenceResId;
+                    c.mPresenceText = entry.mPresenceText;
+                    c.mAvatarData = entry.mAvatarData;
+                    c.mAvatar = entry.mAvatar;
+                    c.mContactMethodId = entry.mContactMethodId;
+                    c.mContactMethodType = entry.mContactMethodType;
+                    c.mNumberE164 = entry.mNumberE164;
+                    c.mName = entry.mName;
+                    c.mSendToVoicemail = entry.mSendToVoicemail;
+
+                    c.notSynchronizedUpdateNameAndNumber();
+
+                    // We saw a bug where we were updating an empty contact. That would trigger
+                    // l.onUpdate() below, which would call ComposeMessageActivity.onUpdate,
+                    // which would call the adapter's notifyDataSetChanged, which would throw
+                    // away the message items and rebuild, eventually calling updateContact()
+                    // again -- all in a vicious and unending loop. Break the cycle and don't
+                    // notify if the number (the most important piece of information) is empty.
+                    if (!TextUtils.isEmpty(c.mNumber)) {
+                        // clone the list of listeners in case the onUpdate call turns around and
+                        // modifies the list of listeners
+                        // access to mListeners is synchronized on ContactsCache
+                        HashSet<UpdateListener> iterator;
+                        synchronized (mListeners) {
+                            iterator = (HashSet<UpdateListener>)Contact.mListeners.clone();
+                        }
+                        for (UpdateListener l : iterator) {
+                            if (Log.isLoggable(LogTag.CONTACT, Log.DEBUG)) {
+                                Log.d(TAG, "updating " + l);
+                            }
+                            l.onUpdate(c);
+                        }
+                    }
+                }
+                synchronized (c) {
+                    c.mQueryPending = false;
+                    c.notifyAll();
+                }
+            }
+        }
+        
+        /**
+         * Returns the caller info in Contact.
+         */
+        private Contact getContactInfo(Contact c) {
+            if (c.mIsMe) {
+                return getContactInfoForSelf();
+            } else if (Mms.isEmailAddress(c.mNumber)) {
+                return getContactInfoForEmailAddress(c.mNumber);
+            } else if (isAlphaNumber(c.mNumber)) {
+                // first try to look it up in the email field
+                Contact contact = getContactInfoForEmailAddress(c.mNumber);
+                if (contact.existsInDatabase()) {
+                    return contact;
+                }
+                // then look it up in the phone field
+                return getContactInfoForPhoneNumber(c.mNumber);
+            } else {
+                // it's a real phone number, so strip out non-digits and look it up
+                final String strippedNumber = PhoneNumberUtils.stripSeparators(c.mNumber);
+                return getContactInfoForPhoneNumber(strippedNumber);
+            }
+        }
+        
+        private Contact internalGet(String numberOrEmail, boolean isMe) {
+            synchronized (ContactsCache.this) {
+                // See if we can find "number" in the hashtable.
+                // If so, just return the result.
+                final boolean isNotRegularPhoneNumber = isMe || Mms.isEmailAddress(numberOrEmail) ||
+                        MessageUtils.isAlias(numberOrEmail);
+                final String key = isNotRegularPhoneNumber ?
+                        numberOrEmail : key(numberOrEmail, sStaticKeyBuffer);
+
+                ArrayList<Contact> candidates = mContactsHash.get(key);
+                if (candidates != null) {
+                    int length = candidates.size();
+                    for (int i = 0; i < length; i++) {
+                        Contact c= candidates.get(i);
+                        if (isNotRegularPhoneNumber) {
+                            if (numberOrEmail.equals(c.mNumber)) {
+                                return c;
+                            }
+                        } else {
+                            if (PhoneNumberUtils.compare(numberOrEmail, c.mNumber)) {
+                                return c;
+                            }
+                        }
+                    }
+                } else {
+                    candidates = new ArrayList<Contact>();
+                    // call toString() since it may be the static CharBuffer
+                    mContactsHash.put(key, candidates);
+                }
+                Contact c = isMe ?
+                        new Contact(true) :
+                        new Contact(numberOrEmail);
+                candidates.add(c);
+                return c;
+            }
         }
     }
     
-    public synchronized String getNumber() {
-        return mNumber;
-    }
-    
-    public synchronized Uri getUri() {
-        return ContentUris.withAppendedId(Contacts.CONTENT_URI, mPersonId);
+    private static void log(String msg) {
+        Log.d(TAG, msg);
     }
 }
