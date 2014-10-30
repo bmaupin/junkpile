d1aeea6079f8a3cd10c40e1ca153d7df66ea4425
bmaupin <bmaupincode@gmail.com>
2014-10-30 13:54:55 -0400

    Add empty PhoneNumberUtils

---
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/android/PhoneNumberUtils.java
@@ -0,0 +1,11 @@
+/*
+ * Derived from android.telephony.PhoneNumberUtils
+ * (tag android-4.4.4_r2.0.1)
+ * https://android.googlesource.com/platform/frameworks/base/+/android-4.4.4_r2.0.1/telephony/java/android/telephony/PhoneNumberUtils.java
+ */
+
+package ca.bmaupin.merge.sms.android;
+
+public class PhoneNumberUtils {
+
+}
