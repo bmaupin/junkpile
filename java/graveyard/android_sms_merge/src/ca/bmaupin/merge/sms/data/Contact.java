0f207b19fddca99fcf213cfe17b4fd5f6c706dae
bmaupin <bmaupin@users.noreply.github.com>
2014-10-30 13:21:43 -0400

    Commit progress on Contact

--- java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/data/Contact.java
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/data/Contact.java
@@ -5,26 +5,37 @@
 
 package ca.bmaupin.merge.sms.data;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 
+import com.android.mms.MmsApp;
 import com.android.mms.data.Contact;
 
 import android.content.ContentUris;
 import android.content.Context;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.provider.ContactsContract.Contacts;
+import android.provider.ContactsContract.Data;
+import android.provider.ContactsContract.Presence;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
+import android.provider.ContactsContract.Profile;
 import android.telephony.PhoneNumberUtils;
 import android.text.TextUtils;
 import android.util.Log;
 import ca.bmaupin.merge.sms.LogTag;
 
 public class Contact {
+	public static final int CONTACT_METHOD_TYPE_PHONE = 1;
+    public static final int CONTACT_METHOD_TYPE_SELF = 3;       // the "Me" or profile contact
+	private static final int CONTACT_METHOD_ID_UNKNOWN = -1;
 	private static final String TAG = "Contact";
 	private static ContactsCache sContactCache;
 	
@@ -38,6 +49,7 @@ public class Contact {
     private String mNumberE164;
     private String mName;
     private String mNameAndNumber;   // for display, e.g. Fred Flintstone <670-782-1123>
+    private boolean mNumberIsModified; // true if the number is modified
     
     private long mRecipientId;       // used to find the Recipient cache entry
     private String mLabel;
@@ -48,10 +60,35 @@ public class Contact {
     private byte [] mAvatarData;
     private boolean mIsStale;
     private boolean mQueryPending;
+    private boolean mIsMe;          // true if this contact is me!
     private boolean mSendToVoicemail;   // true if this contact should not put up notification
     
     public interface UpdateListener {
         public void onUpdate(Contact updated);
+    }
+    
+    /*
+     * Make a basic contact object with a phone number.
+     */
+    private Contact(String number) {
+        init(number, "");
+    }
+
+    private Contact(boolean isMe) {
+        init(SELF_ITEM_KEY, "");
+        mIsMe = isMe;
+    }
+    
+    private void init(String number, String name) {
+        mContactMethodId = CONTACT_METHOD_ID_UNKNOWN;
+        mName = name;
+        setNumber(number);
+        mNumberIsModified = false;
+        mLabel = "";
+        mPersonId = 0;
+        mPresenceResId = 0;
+        mIsStale = true;
+        mSendToVoicemail = false;
     }
 	
     public static void logWithTrace(String tag, String msg, Object... format) {
@@ -120,6 +157,17 @@ public class Contact {
         return mNumber;
     }
     
+    public synchronized void setNumber(String number) {
+        if (!Mms.isEmailAddress(number)) {
+            mNumber = PhoneNumberUtils.formatNumber(number, mNumberE164,
+                    MmsApp.getApplication().getCurrentCountryIso());
+        } else {
+            mNumber = number;
+        }
+        notSynchronizedUpdateNameAndNumber();
+        mNumberIsModified = true;
+    }
+    
     public synchronized String getName() {
         if (TextUtils.isEmpty(mName)) {
             return mNumber;
@@ -153,6 +201,74 @@ public class Contact {
     private static class ContactsCache {
     	private final TaskStack mTaskQueue = new TaskStack();
     	
+        /**
+         * For a specified phone number, 2 rows were inserted into phone_lookup
+         * table. One is the phone number's E164 representation, and another is
+         * one's normalized format. If the phone number's normalized format in
+         * the lookup table is the suffix of the given number's one, it is
+         * treated as matched CallerId. E164 format number must fully equal.
+         *
+         * For example: Both 650-123-4567 and +1 (650) 123-4567 will match the
+         * normalized number 6501234567 in the phone lookup.
+         *
+         *  The min_match is used to narrow down the candidates for the final
+         * comparison.
+         */
+        // query params for caller id lookup
+        private static final String CALLER_ID_SELECTION = " Data._ID IN "
+                + " (SELECT DISTINCT lookup.data_id "
+                + " FROM "
+                    + " (SELECT data_id, normalized_number, length(normalized_number) as len "
+                    + " FROM phone_lookup "
+                    + " WHERE min_match = ?) AS lookup "
+                + " WHERE lookup.normalized_number = ? OR"
+                    + " (lookup.len <= ? AND "
+                        + " substr(?, ? - lookup.len + 1) = lookup.normalized_number))";
+
+        // query params for caller id lookup without E164 number as param
+        private static final String CALLER_ID_SELECTION_WITHOUT_E164 =  " Data._ID IN "
+            + " (SELECT DISTINCT lookup.data_id "
+            + " FROM "
+                + " (SELECT data_id, normalized_number, length(normalized_number) as len "
+                + " FROM phone_lookup "
+                + " WHERE min_match = ?) AS lookup "
+            + " WHERE "
+                + " (lookup.len <= ? AND "
+                    + " substr(?, ? - lookup.len + 1) = lookup.normalized_number))";
+
+        // Utilizing private API
+        private static final Uri PHONES_WITH_PRESENCE_URI = Data.CONTENT_URI;
+
+        private static final String[] CALLER_ID_PROJECTION = new String[] {
+                Phone._ID,                      // 0
+                Phone.NUMBER,                   // 1
+                Phone.LABEL,                    // 2
+                Phone.DISPLAY_NAME,             // 3
+                Phone.CONTACT_ID,               // 4
+                Phone.CONTACT_PRESENCE,         // 5
+                Phone.CONTACT_STATUS,           // 6
+                Phone.NORMALIZED_NUMBER,        // 7
+                Contacts.SEND_TO_VOICEMAIL      // 8
+        };
+        
+        private static final int PHONE_ID_COLUMN = 0;
+        private static final int PHONE_LABEL_COLUMN = 2;
+        private static final int CONTACT_NAME_COLUMN = 3;
+        private static final int CONTACT_ID_COLUMN = 4;
+        private static final int CONTACT_PRESENCE_COLUMN = 5;
+        private static final int CONTACT_STATUS_COLUMN = 6;
+        private static final int PHONE_NORMALIZED_NUMBER = 7;
+        private static final int SEND_TO_VOICEMAIL = 8;
+        
+        private static final String[] SELF_PROJECTION = new String[] {
+            Phone._ID,                      // 0
+            Phone.DISPLAY_NAME,             // 1
+        };
+        
+        private static final int SELF_NAME_COLUMN = 1;
+    	
+        private final Context mContext;
+        
         private static class TaskStack {
             Thread mWorkerThread;
             private final ArrayList<Runnable> mThingsToLoad;
@@ -403,6 +519,271 @@ public class Contact {
                 final String strippedNumber = PhoneNumberUtils.stripSeparators(c.mNumber);
                 return getContactInfoForPhoneNumber(strippedNumber);
             }
+        }
+        
+        // Some received sms's have addresses such as "OakfieldCPS" or "T-Mobile". This
+        // function will attempt to identify these and return true. If the number contains
+        // 3 or more digits, such as "jello123", this function will return false.
+        // Some countries have 3 digits shortcodes and we have to identify them as numbers.
+        //    http://en.wikipedia.org/wiki/Short_code
+        // Examples of input/output for this function:
+        //    "Jello123" -> false  [3 digits, it is considered to be the phone number "123"]
+        //    "T-Mobile" -> true   [it is considered to be the address "T-Mobile"]
+        //    "Mobile1"  -> true   [1 digit, it is considered to be the address "Mobile1"]
+        //    "Dogs77"   -> true   [2 digits, it is considered to be the address "Dogs77"]
+        //    "****1"    -> true   [1 digits, it is considered to be the address "****1"]
+        //    "#4#5#6#"  -> true   [it is considered to be the address "#4#5#6#"]
+        //    "AB12"     -> true   [2 digits, it is considered to be the address "AB12"]
+        //    "12"       -> true   [2 digits, it is considered to be the address "12"]
+        private boolean isAlphaNumber(String number) {
+            // TODO: PhoneNumberUtils.isWellFormedSmsAddress() only check if the number is a valid
+            // GSM SMS address. If the address contains a dialable char, it considers it a well
+            // formed SMS addr. CDMA doesn't work that way and has a different parser for SMS
+            // address (see CdmaSmsAddress.parse(String address)). We should definitely fix this!!!
+            if (!PhoneNumberUtils.isWellFormedSmsAddress(number)) {
+                // The example "T-Mobile" will exit here because there are no numbers.
+                return true;        // we're not an sms address, consider it an alpha number
+            }
+            if (MessageUtils.isAlias(number)) {
+                return true;
+            }
+            number = PhoneNumberUtils.extractNetworkPortion(number);
+            if (TextUtils.isEmpty(number)) {
+                return true;    // there are no digits whatsoever in the number
+            }
+            // At this point, anything like "Mobile1" or "Dogs77" will be stripped down to
+            // "1" and "77". "#4#5#6#" remains as "#4#5#6#" at this point.
+            return number.length() < 3;
+        }
+        
+        /**
+         * Queries the caller id info with the phone number.
+         * @return a Contact containing the caller id info corresponding to the number.
+         */
+        private Contact getContactInfoForPhoneNumber(String number) {
+            Contact entry = new Contact(number);
+            entry.mContactMethodType = CONTACT_METHOD_TYPE_PHONE;
+
+            if (Log.isLoggable(LogTag.CONTACT, Log.DEBUG)) {
+                log("queryContactInfoByNumber: number=" + number);
+            }
+
+            String normalizedNumber = PhoneNumberUtils.normalizeNumber(number);
+            String minMatch = PhoneNumberUtils.toCallerIDMinMatch(normalizedNumber);
+            if (!TextUtils.isEmpty(normalizedNumber) && !TextUtils.isEmpty(minMatch)) {
+                String numberLen = String.valueOf(normalizedNumber.length());
+                String numberE164 = PhoneNumberUtils.formatNumberToE164(
+                        number, MmsApp.getApplication().getCurrentCountryIso());
+                String selection;
+                String[] args;
+                if (TextUtils.isEmpty(numberE164)) {
+                    selection = CALLER_ID_SELECTION_WITHOUT_E164;
+                    args = new String[] {minMatch, numberLen, normalizedNumber, numberLen};
+                } else {
+                    selection = CALLER_ID_SELECTION;
+                    args = new String[] {
+                            minMatch, numberE164, numberLen, normalizedNumber, numberLen};
+                }
+
+                Cursor cursor = mContext.getContentResolver().query(
+                        PHONES_WITH_PRESENCE_URI, CALLER_ID_PROJECTION, selection, args, null);
+                if (cursor == null) {
+                    Log.w(TAG, "queryContactInfoByNumber(" + number + ") returned NULL cursor!"
+                            + " contact uri used " + PHONES_WITH_PRESENCE_URI);
+                    return entry;
+                }
+
+                try {
+                    if (cursor.moveToFirst()) {
+                        fillPhoneTypeContact(entry, cursor);
+                    }
+                } finally {
+                    cursor.close();
+                }
+            }
+            return entry;
+        }
+        
+        /**
+         * @return a Contact containing the info for the profile.
+         */
+        private Contact getContactInfoForSelf() {
+            Contact entry = new Contact(true);
+            entry.mContactMethodType = CONTACT_METHOD_TYPE_SELF;
+
+            if (Log.isLoggable(LogTag.CONTACT, Log.DEBUG)) {
+                log("getContactInfoForSelf");
+            }
+            Cursor cursor = mContext.getContentResolver().query(
+                    Profile.CONTENT_URI, SELF_PROJECTION, null, null, null);
+            if (cursor == null) {
+                Log.w(TAG, "getContactInfoForSelf() returned NULL cursor!"
+                        + " contact uri used " + Profile.CONTENT_URI);
+                return entry;
+            }
+
+            try {
+                if (cursor.moveToFirst()) {
+                    fillSelfContact(entry, cursor);
+                }
+            } finally {
+                cursor.close();
+            }
+            return entry;
+        }
+        
+        private void fillPhoneTypeContact(final Contact contact, final Cursor cursor) {
+            synchronized (contact) {
+                contact.mContactMethodType = CONTACT_METHOD_TYPE_PHONE;
+                contact.mContactMethodId = cursor.getLong(PHONE_ID_COLUMN);
+                contact.mLabel = cursor.getString(PHONE_LABEL_COLUMN);
+                contact.mName = cursor.getString(CONTACT_NAME_COLUMN);
+                contact.mPersonId = cursor.getLong(CONTACT_ID_COLUMN);
+                contact.mPresenceResId = getPresenceIconResourceId(
+                        cursor.getInt(CONTACT_PRESENCE_COLUMN));
+                contact.mPresenceText = cursor.getString(CONTACT_STATUS_COLUMN);
+                contact.mNumberE164 = cursor.getString(PHONE_NORMALIZED_NUMBER);
+                contact.mSendToVoicemail = cursor.getInt(SEND_TO_VOICEMAIL) == 1;
+                if (Log.isLoggable(LogTag.CONTACT, Log.DEBUG)) {
+                    log("fillPhoneTypeContact: name=" + contact.mName + ", number="
+                            + contact.mNumber + ", presence=" + contact.mPresenceResId
+                            + " SendToVoicemail: " + contact.mSendToVoicemail);
+                }
+            }
+            byte[] data = loadAvatarData(contact);
+
+            synchronized (contact) {
+                contact.mAvatarData = data;
+            }
+        }
+        
+        private void fillSelfContact(final Contact contact, final Cursor cursor) {
+            synchronized (contact) {
+                contact.mName = cursor.getString(SELF_NAME_COLUMN);
+                if (TextUtils.isEmpty(contact.mName)) {
+                    contact.mName = mContext.getString(R.string.messagelist_sender_self);
+                }
+                if (Log.isLoggable(LogTag.CONTACT, Log.DEBUG)) {
+                    log("fillSelfContact: name=" + contact.mName + ", number="
+                            + contact.mNumber);
+                }
+            }
+            byte[] data = loadAvatarData(contact);
+
+            synchronized (contact) {
+                contact.mAvatarData = data;
+            }
+        }
+        /*
+         * Load the avatar data from the cursor into memory.  Don't decode the data
+         * until someone calls for it (see getAvatar).  Hang onto the raw data so that
+         * we can compare it when the data is reloaded.
+         * TODO: consider comparing a checksum so that we don't have to hang onto
+         * the raw bytes after the image is decoded.
+         */
+        private byte[] loadAvatarData(Contact entry) {
+            byte [] data = null;
+
+            if ((!entry.mIsMe && entry.mPersonId == 0) || entry.mAvatar != null) {
+                return null;
+            }
+
+            if (Log.isLoggable(LogTag.CONTACT, Log.DEBUG)) {
+                log("loadAvatarData: name=" + entry.mName + ", number=" + entry.mNumber);
+            }
+
+            // If the contact is "me", then use my local profile photo. Otherwise, build a
+            // uri to get the avatar of the contact.
+            Uri contactUri = entry.mIsMe ?
+                    Profile.CONTENT_URI :
+                    ContentUris.withAppendedId(Contacts.CONTENT_URI, entry.mPersonId);
+
+            InputStream avatarDataStream = Contacts.openContactPhotoInputStream(
+                        mContext.getContentResolver(),
+                        contactUri);
+            try {
+                if (avatarDataStream != null) {
+                    data = new byte[avatarDataStream.available()];
+                    avatarDataStream.read(data, 0, data.length);
+                }
+            } catch (IOException ex) {
+                //
+            } finally {
+                try {
+                    if (avatarDataStream != null) {
+                        avatarDataStream.close();
+                    }
+                } catch (IOException e) {
+                }
+            }
+
+            return data;
+        }
+        
+        private int getPresenceIconResourceId(int presence) {
+            // TODO: must fix for SDK
+            if (presence != Presence.OFFLINE) {
+                return Presence.getPresenceIconResourceId(presence);
+            }
+
+            return 0;
+        }
+        
+        /**
+         * Query the contact email table to get the name of an email address.
+         */
+        private Contact getContactInfoForEmailAddress(String email) {
+            Contact entry = new Contact(email);
+            entry.mContactMethodType = CONTACT_METHOD_TYPE_EMAIL;
+
+            Cursor cursor = SqliteWrapper.query(mContext, mContext.getContentResolver(),
+                    EMAIL_WITH_PRESENCE_URI,
+                    EMAIL_PROJECTION,
+                    EMAIL_SELECTION,
+                    new String[] { email },
+                    null);
+
+            if (cursor != null) {
+                try {
+                    while (cursor.moveToNext()) {
+                        boolean found = false;
+                        synchronized (entry) {
+                            entry.mContactMethodId = cursor.getLong(EMAIL_ID_COLUMN);
+                            entry.mPresenceResId = getPresenceIconResourceId(
+                                    cursor.getInt(EMAIL_STATUS_COLUMN));
+                            entry.mPersonId = cursor.getLong(EMAIL_CONTACT_ID_COLUMN);
+                            entry.mSendToVoicemail =
+                                    cursor.getInt(EMAIL_SEND_TO_VOICEMAIL_COLUMN) == 1;
+
+                            String name = cursor.getString(EMAIL_NAME_COLUMN);
+                            if (TextUtils.isEmpty(name)) {
+                                name = cursor.getString(EMAIL_CONTACT_NAME_COLUMN);
+                            }
+                            if (!TextUtils.isEmpty(name)) {
+                                entry.mName = name;
+                                if (Log.isLoggable(LogTag.CONTACT, Log.DEBUG)) {
+                                    log("getContactInfoForEmailAddress: name=" + entry.mName +
+                                            ", email=" + email + ", presence=" +
+                                            entry.mPresenceResId);
+                                }
+                                found = true;
+                            }
+                        }
+
+                        if (found) {
+                            byte[] data = loadAvatarData(entry);
+                            synchronized (entry) {
+                                entry.mAvatarData = data;
+                            }
+
+                            break;
+                        }
+                    }
+                } finally {
+                    cursor.close();
+                }
+            }
+            return entry;
         }
         
         private Contact internalGet(String numberOrEmail, boolean isMe) {
