d308d2c6d023c03a36de16f89049eed96892b3c2
bmaupin <bmaupin@users.noreply.github.com>
2014-10-30 13:37:57 -0400

    Commit barebones Telephony.java

---
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/android/Telephony.java
@@ -0,0 +1,18 @@
+/*
+ * Derived from android.provider.Telephony
+ * (tag android-4.4.4_r2.0.1)
+ * https://android.googlesource.com/platform/frameworks/opt/telephony/+/android-4.4.4_r2.0.1/src/java/android/provider/Telephony.java
+ */
+
+package ca.bmaupin.merge.sms.android;
+
+import android.provider.Telephony.BaseMmsColumns;
+
+public class Telephony {
+    /**
+     * Contains all MMS messages.
+     */
+    public static final class Mms implements BaseMmsColumns {
+    	
+    }
+}
