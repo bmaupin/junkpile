7c71ac6c1ef751160213ce75d38184379c41e9bc
bmaupin <bmaupincode@gmail.com>
2014-10-30 14:13:48 -0400

    Add another dependency for Contact

--- java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/android/PhoneNumberUtils.java
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/android/PhoneNumberUtils.java
@@ -6,6 +6,8 @@
 
 package ca.bmaupin.merge.sms.android;
 
+import android.text.TextUtils;
+
 public class PhoneNumberUtils {
     /*
      * Special characters
@@ -18,12 +20,70 @@ public class PhoneNumberUtils {
     public static final char PAUSE = ',';
     public static final char WAIT = ';';
     public static final char WILD = 'N';
+    /*
+     * Calling Line Identification Restriction (CLIR)
+     */
+    private static final String CLIR_ON = "*31#";
+    private static final String CLIR_OFF = "#31#";
 	
+    /** True if c is ISO-LATIN characters 0-9, *, # , +, WILD  */
+    public final static boolean
+    isDialable(char c) {
+        return (c >= '0' && c <= '9') || c == '*' || c == '#' || c == '+' || c == WILD;
+    }
+    
     /** True if c is ISO-LATIN characters 0-9, *, # , +, WILD, WAIT, PAUSE   */
     public final static boolean
     isNonSeparator(char c) {
         return (c >= '0' && c <= '9') || c == '*' || c == '#' || c == '+'
                 || c == WILD || c == WAIT || c == PAUSE;
+    }
+    
+    /** This any anything to the right of this char is part of the
+     *  post-dial string (eg this is PAUSE or WAIT)
+     */
+    public final static boolean
+    isStartsPostDial (char c) {
+        return c == PAUSE || c == WAIT;
+    }
+    
+    
+    /** Extracts the network address portion and canonicalizes
+     *  (filters out separators.)
+     *  Network address portion is everything up to DTMF control digit
+     *  separators (pause or wait), but without non-dialable characters.
+     *
+     *  Please note that the GSM wild character is allowed in the result.
+     *  This must be resolved before dialing.
+     *
+     *  Returns null if phoneNumber == null
+     */
+    public static String
+    extractNetworkPortion(String phoneNumber) {
+        if (phoneNumber == null) {
+            return null;
+        }
+        int len = phoneNumber.length();
+        StringBuilder ret = new StringBuilder(len);
+        for (int i = 0; i < len; i++) {
+            char c = phoneNumber.charAt(i);
+            // Character.digit() supports ASCII and Unicode digits (fullwidth, Arabic-Indic, etc.)
+            int digit = Character.digit(c, 10);
+            if (digit != -1) {
+                ret.append(digit);
+            } else if (c == '+') {
+                // Allow '+' as first character or after CLIR MMI prefix
+                String prefix = ret.toString();
+                if (prefix.length() == 0 || prefix.equals(CLIR_ON) || prefix.equals(CLIR_OFF)) {
+                    ret.append(c);
+                }
+            } else if (isDialable(c)) {
+                ret.append(c);
+            } else if (isStartsPostDial (c)) {
+                break;
+            }
+        }
+        return ret.toString();
     }
 	
 	/**
@@ -48,5 +108,27 @@ public class PhoneNumberUtils {
             }
         }
         return ret.toString();
+    }
+    
+    /**
+     * Return true iff the network portion of <code>address</code> is,
+     * as far as we can tell on the device, suitable for use as an SMS
+     * destination address.
+     */
+    public static boolean isWellFormedSmsAddress(String address) {
+        String networkPortion =
+                PhoneNumberUtils.extractNetworkPortion(address);
+        return (!(networkPortion.equals("+")
+                  || TextUtils.isEmpty(networkPortion)))
+               && isDialable(networkPortion);
+    }
+    
+    private static boolean isDialable(String address) {
+        for (int i = 0, count = address.length(); i < count; i++) {
+            if (!isDialable(address.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
