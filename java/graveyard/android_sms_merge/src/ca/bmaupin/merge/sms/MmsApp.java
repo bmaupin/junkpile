daaf2b4dfc382037f2e2cfa35ee66d5d5f0d36dd
bmaupin <bmaupincode@gmail.com>
2014-10-31 14:20:42 -0400

    Back to square one :P Add application so we can access application context

---
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/MmsApp.java
@@ -0,0 +1,12 @@
+/*
+ * Derived from com.android.mms.MmsApp
+ * (tag android-4.4.4_r2.0.1)
+ */
+
+package ca.bmaupin.merge.sms;
+
+import android.app.Application;
+
+public class MmsApp extends Application {
+
+}
