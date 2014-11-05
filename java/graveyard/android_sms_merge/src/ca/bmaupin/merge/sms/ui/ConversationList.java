a97ae8934ac49bd9543d617671d94e6a03d3c47c
bmaupin <bmaupin@users.noreply.github.com>
2014-11-05 14:20:11 -0500

    Go ahead and use Conversation.from() since we've implemented the cache

--- java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/ui/ConversationList.java
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/ui/ConversationList.java
@@ -5,8 +5,60 @@
 
 package ca.bmaupin.merge.sms.ui;
 
+import ca.bmaupin.merge.sms.R;
+import ca.bmaupin.merge.sms.data.Contact;
+import ca.bmaupin.merge.sms.data.Conversation;
 import android.app.ListActivity;
+import android.content.AsyncQueryHandler;
+import android.content.ContentResolver;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SqliteWrapper;
+import android.os.Bundle;
+import android.widget.TextView;
 
 public class ConversationList extends ListActivity {
+    private static final int THREAD_LIST_QUERY_TOKEN       = 1701;
+	
+    private ThreadListQueryHandler mQueryHandler;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.conversation_list_screen);
+    }
+    
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        startAsyncQuery();
+
+        // we invalidate the contact cache here because we want to get updated presence
+        // and any contact changes. We don't invalidate the cache by observing presence and contact
+        // changes (since that's too untargeted), so as a tradeoff we do it here.
+        // If we're in the middle of the app initialization where we're loading the conversation
+        // threads, don't invalidate the cache because we're in the process of building it.
+        // TODO: think of a better way to invalidate cache more surgically or based on actual
+        // TODO: changes we care about
+        if (!Conversation.loadingThreads()) {
+            Contact.invalidateCache();
+        }
+    }
+    
+    private void startAsyncQuery() {
+        try {
+            ((TextView)(getListView().getEmptyView())).setText(R.string.loading_conversations);
+
+            Conversation.startQueryForAll(mQueryHandler, THREAD_LIST_QUERY_TOKEN);
+        } catch (SQLiteException e) {
+            SqliteWrapper.checkSQLiteException(this, e);
+        }
+    }
+    
+    private final class ThreadListQueryHandler extends AsyncQueryHandler {
+        public ThreadListQueryHandler(ContentResolver contentResolver) {
+            super(contentResolver);
+        }
+    }
 }
