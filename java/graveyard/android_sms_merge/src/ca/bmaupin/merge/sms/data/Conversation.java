e685d51f6daffe538a2e06365705b5a2569b5047
bmaupin <bmaupin@users.noreply.github.com>
2014-11-05 14:20:34 -0500

    Commit progress

--- java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/data/Conversation.java
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/data/Conversation.java
@@ -64,6 +64,38 @@ public class Conversation {
     }
     
     /**
+     * Returns a temporary Conversation (not representing one on disk) wrapping
+     * the contents of the provided cursor.  The cursor should be the one
+     * returned to your AsyncQueryHandler passed in to {@link #startQueryForAll}.
+     * The recipient list of this conversation can be empty if the results
+     * were not in cache.
+     */
+    public static Conversation from(Context context, Cursor cursor) {
+        // First look in the cache for the Conversation and return that one. That way, all the
+        // people that are looking at the cached copy will get updated when fillFromCursor() is
+        // called with this cursor.
+        long threadId = cursor.getLong(ID);
+        if (threadId > 0) {
+            Conversation conv = Cache.get(threadId);
+            if (conv != null) {
+                fillFromCursor(context, conv, cursor, false);   // update the existing conv in-place
+                return conv;
+            }
+        }
+        Conversation conv = new Conversation(context, cursor, false);
+        try {
+            Cache.put(conv);
+        } catch (IllegalStateException e) {
+            LogTag.error(TAG, "Tried to add duplicate Conversation to Cache (from cursor): " +
+                    conv);
+            if (!Cache.replace(conv)) {
+                LogTag.error("Converations.from cache.replace failed on " + conv);
+            }
+        }
+        return conv;
+    }
+    
+    /**
      * Returns the thread ID of this conversation.  Can be zero if
      * {@link #ensureThreadId} has not been called yet.
      */
