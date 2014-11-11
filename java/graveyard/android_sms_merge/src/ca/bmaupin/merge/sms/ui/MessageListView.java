2d307ed8834549b2321f76e3d3e149379606c47e
bmaupin <bmaupin@users.noreply.github.com>
2014-11-11 13:41:46 -0500

    Add constructors

--- java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/ui/MessageListView.java
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/ui/MessageListView.java
@@ -5,8 +5,16 @@
 
 package ca.bmaupin.merge.sms.ui;
 
+import android.content.Context;
+import android.util.AttributeSet;
 import android.widget.ListView;
 
 public final class MessageListView extends ListView {
+    public MessageListView(Context context) {
+        super(context);
+    }
 
+    public MessageListView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
 }
