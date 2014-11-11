8771a60a84475190df300273abc3640b8b137107
bmaupin <bmaupincode@gmail.com>
2014-11-11 13:39:45 -0500

    Add empty MessageListView

---
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/ui/MessageListView.java
@@ -0,0 +1,12 @@
+/*
+ * Derived from com.android.mms.ui.MessageListView
+ * (tag android-4.4.4_r2.0.1)
+ */
+
+package ca.bmaupin.merge.sms.ui;
+
+import android.widget.ListView;
+
+public final class MessageListView extends ListView {
+
+}
