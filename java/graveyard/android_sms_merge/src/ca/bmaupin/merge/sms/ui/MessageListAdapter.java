8fe90bf3b585105e9303be636031cbad589ff367
bmaupin <bmaupincode@gmail.com>
2014-11-11 13:43:01 -0500

    Add empty MessageListAdapter

---
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/ui/MessageListAdapter.java
@@ -0,0 +1,15 @@
+/*
+ * Derived from com.android.mms.ui.MessageListAdapter
+ * (tag android-4.4.4_r2.0.1)
+ */
+
+package ca.bmaupin.merge.sms.ui;
+
+import android.widget.CursorAdapter;
+
+/**
+ * The back-end data adapter of a message list.
+ */
+public class MessageListAdapter extends CursorAdapter {
+
+}
