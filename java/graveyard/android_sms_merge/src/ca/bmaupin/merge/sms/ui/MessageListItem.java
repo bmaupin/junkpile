f95b8f5ae4828cf5e4842c0c0bc857ce9c5b247a
bmaupin <bmaupincode@gmail.com>
2014-11-13 13:34:41 -0500

    Add empty MessageListItem

---
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/ui/MessageListItem.java
@@ -0,0 +1,12 @@
+/*
+ * Derived from com.android.mms.ui.MessageListItem
+ * (tag android-4.4.4_r2.0.1)
+ */
+
+package ca.bmaupin.merge.sms.ui;
+
+import android.widget.LinearLayout;
+
+public class MessageListItem extends LinearLayout {
+
+}
