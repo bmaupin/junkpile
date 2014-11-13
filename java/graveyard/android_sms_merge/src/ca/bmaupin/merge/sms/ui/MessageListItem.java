22a88dd192bf9eac46f156b0d1bc391f9e96bab4
bmaupin <bmaupin@users.noreply.github.com>
2014-11-13 13:40:38 -0500

    Add constructors

--- java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/ui/MessageListItem.java
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/ui/MessageListItem.java
@@ -5,8 +5,39 @@
 
 package ca.bmaupin.merge.sms.ui;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.text.style.ForegroundColorSpan;
+import android.util.AttributeSet;
 import android.widget.LinearLayout;
 
-public class MessageListItem extends LinearLayout {
+import ca.bmaupin.merge.sms.MmsApp;
+import ca.bmaupin.merge.sms.R;
 
+public class MessageListItem extends LinearLayout {
+    private String mDefaultCountryIso;
+    static private Drawable sDefaultContactImage;
+	
+    public MessageListItem(Context context) {
+        super(context);
+        mDefaultCountryIso = MmsApp.getApplication().getCurrentCountryIso();
+
+        if (sDefaultContactImage == null) {
+            sDefaultContactImage = context.getResources().getDrawable(R.drawable.ic_contact_picture);
+        }
+    }
+
+    public MessageListItem(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        int color = mContext.getResources().getColor(R.color.timestamp_color);
+        mColorSpan = new ForegroundColorSpan(color);
+        mDefaultCountryIso = MmsApp.getApplication().getCurrentCountryIso();
+
+        if (sDefaultContactImage == null) {
+            sDefaultContactImage = context.getResources().getDrawable(R.drawable.ic_contact_picture);
+        }
+    }
+    
+    ForegroundColorSpan mColorSpan = null;  // set in ctor
 }
