170a7ecb1d12403ca9d443a644a3664f975d6e1e
bmaupin <bmaupincode@gmail.com>
2014-11-14 14:26:24 -0500

    Commit progress

--- java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/ui/MessageListItem.java
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/ui/MessageListItem.java
@@ -5,18 +5,49 @@
 
 package ca.bmaupin.merge.sms.ui;
 
-import android.content.Context;
-import android.graphics.drawable.Drawable;
-import android.text.style.ForegroundColorSpan;
-import android.util.AttributeSet;
-import android.widget.LinearLayout;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import com.google.android.mms.pdu.PduHeaders;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.provider.ContactsContract.Profile;
+import android.provider.Telephony.Sms;
+import android.telephony.TelephonyManager;
+import android.text.Html;
+import android.text.SpannableStringBuilder;
+import android.text.TextUtils;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.QuickContactBadge;
+import android.widget.TextView;
 import ca.bmaupin.merge.sms.MmsApp;
 import ca.bmaupin.merge.sms.R;
+import ca.bmaupin.merge.sms.data.Contact;
 
 public class MessageListItem extends LinearLayout {
+	private static final String TAG = "MessageListItem";
+	private static final boolean DEBUG = false;
+	
+	private ImageView mImageView;
+	private TextView mBodyTextView;
+	private MessageItem mMessageItem;
     private String mDefaultCountryIso;
+    private QuickContactBadge mAvatar;
     static private Drawable sDefaultContactImage;
+    private int mPosition;      // for debugging
+    private boolean mMultiRecipients;
 	
     public MessageListItem(Context context) {
         super(context);
@@ -39,5 +70,245 @@ public class MessageListItem extends LinearLayout {
         }
     }
     
+    public void bind(MessageItem msgItem, boolean convHasMultiRecipients, int position) {
+        if (DEBUG) {
+            Log.v(TAG, "bind for item: " + position + " old: " +
+                   (mMessageItem != null ? mMessageItem.toString() : "NULL" ) +
+                    " new " + msgItem.toString());
+        }
+        boolean sameItem = mMessageItem != null && mMessageItem.mMsgId == msgItem.mMsgId;
+        mMessageItem = msgItem;
+
+        mPosition = position;
+        mMultiRecipients = convHasMultiRecipients;
+
+        setLongClickable(false);
+        setClickable(false);    // let the list view handle clicks on the item normally. When
+                                // clickable is true, clicks bypass the listview and go straight
+                                // to this listitem. We always want the listview to handle the
+                                // clicks first.
+
+        switch (msgItem.mMessageType) {
+            default:
+                bindCommonMessage(sameItem);
+                break;
+        }
+    }
+    
+    public void unbind() {
+        // Clear all references to the message item, which can contain attachments and other
+        // memory-intensive objects
+        if (mImageView != null) {
+            // Because #setOnClickListener may have set the listener to an object that has the
+            // message item in its closure.
+            mImageView.setOnClickListener(null);
+        }
+        if (mSlideShowButton != null) {
+            // Because #drawPlaybackButton sets the tag to mMessageItem
+            mSlideShowButton.setTag(null);
+        }
+        // leave the presenter in case it's needed when rebound to a different MessageItem.
+        if (mPresenter != null) {
+            mPresenter.cancelBackgroundLoading();
+        }
+    }
+    
+    private void updateAvatarView(String addr, boolean isSelf) {
+        Drawable avatarDrawable;
+        if (isSelf || !TextUtils.isEmpty(addr)) {
+            Contact contact = isSelf ? Contact.getMe(false) : Contact.get(addr, false);
+            avatarDrawable = contact.getAvatar(mContext, sDefaultContactImage);
+
+            if (isSelf) {
+                mAvatar.assignContactUri(Profile.CONTENT_URI);
+            } else {
+                if (contact.existsInDatabase()) {
+                    mAvatar.assignContactUri(contact.getUri());
+                } else {
+                    mAvatar.assignContactFromPhone(contact.getNumber(), true);
+                }
+            }
+        } else {
+            avatarDrawable = sDefaultContactImage;
+        }
+        mAvatar.setImageDrawable(avatarDrawable);
+    }
+    
+    private void bindCommonMessage(final boolean sameItem) {
+        // Since the message text should be concatenated with the sender's
+        // address(or name), I have to display it here instead of
+        // displaying it by the Presenter.
+        mBodyTextView.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+
+        boolean haveLoadedPdu = mMessageItem.isSms() || mMessageItem.mSlideshow != null;
+        // Here we're avoiding reseting the avatar to the empty avatar when we're rebinding
+        // to the same item. This happens when there's a DB change which causes the message item
+        // cache in the MessageListAdapter to get cleared. When an mms MessageItem is newly
+        // created, it has no info in it except the message id. The info is eventually loaded
+        // and bindCommonMessage is called again (see onPduLoaded below). When we haven't loaded
+        // the pdu, we don't want to call updateAvatarView because it
+        // will set the avatar to the generic avatar then when this method is called again
+        // from onPduLoaded, it will reset to the real avatar. This test is to avoid that flash.
+        if (!sameItem || haveLoadedPdu) {
+            boolean isSelf = Sms.isOutgoingFolder(mMessageItem.mBoxId);
+            String addr = isSelf ? null : mMessageItem.mAddress;
+            updateAvatarView(addr, isSelf);
+        }
+
+        // Get and/or lazily set the formatted message from/on the
+        // MessageItem.  Because the MessageItem instances come from a
+        // cache (currently of size ~50), the hit rate on avoiding the
+        // expensive formatMessage() call is very high.
+        CharSequence formattedMessage = mMessageItem.getCachedFormattedMessage();
+        if (formattedMessage == null) {
+            formattedMessage = formatMessage(mMessageItem,
+                                             mMessageItem.mBody,
+                                             mMessageItem.mSubject,
+                                             mMessageItem.mHighlight,
+                                             mMessageItem.mTextContentType);
+            mMessageItem.setCachedFormattedMessage(formattedMessage);
+        }
+        if (!sameItem || haveLoadedPdu) {
+            mBodyTextView.setText(formattedMessage);
+        }
+
+        // Debugging code to put the URI of the image attachment in the body of the list item.
+        if (DEBUG) {
+            String debugText = null;
+            if (mMessageItem.mSlideshow == null) {
+                debugText = "NULL slideshow";
+            } else {
+                SlideModel slide = mMessageItem.mSlideshow.get(0);
+                if (slide == null) {
+                    debugText = "NULL first slide";
+                } else if (!slide.hasImage()) {
+                    debugText = "Not an image";
+                } else {
+                    debugText = slide.getImage().getUri().toString();
+                }
+            }
+            mBodyTextView.setText(mPosition + ": " + debugText);
+        }
+
+        // If we're in the process of sending a message (i.e. pending), then we show a "SENDING..."
+        // string in place of the timestamp.
+        if (!sameItem || haveLoadedPdu) {
+            mDateView.setText(buildTimestampLine(mMessageItem.isSending() ?
+                    mContext.getResources().getString(R.string.sending_message) :
+                        mMessageItem.mTimestamp));
+        }
+        if (mMessageItem.isSms()) {
+            showMmsView(false);
+            mMessageItem.setOnPduLoaded(null);
+        } else {
+            if (DEBUG) {
+                Log.v(TAG, "bindCommonMessage for item: " + mPosition + " " +
+                        mMessageItem.toString() +
+                        " mMessageItem.mAttachmentType: " + mMessageItem.mAttachmentType +
+                        " sameItem: " + sameItem);
+            }
+            if (mMessageItem.mAttachmentType != WorkingMessage.TEXT) {
+                if (!sameItem) {
+                    setImage(null, null);
+                }
+                setOnClickListener(mMessageItem);
+                drawPlaybackButton(mMessageItem);
+            } else {
+                showMmsView(false);
+            }
+            if (mMessageItem.mSlideshow == null) {
+                mMessageItem.setOnPduLoaded(new MessageItem.PduLoadedCallback() {
+                    public void onPduLoaded(MessageItem messageItem) {
+                        if (DEBUG) {
+                            Log.v(TAG, "PduLoadedCallback in MessageListItem for item: " + mPosition +
+                                    " " + (mMessageItem == null ? "NULL" : mMessageItem.toString()) +
+                                    " passed in item: " +
+                                    (messageItem == null ? "NULL" : messageItem.toString()));
+                        }
+                        if (messageItem != null && mMessageItem != null &&
+                                messageItem.getMessageId() == mMessageItem.getMessageId()) {
+                            mMessageItem.setCachedFormattedMessage(null);
+                            bindCommonMessage(true);
+                        }
+                    }
+                });
+            } else {
+                if (mPresenter == null) {
+                    mPresenter = PresenterFactory.getPresenter(
+                            "MmsThumbnailPresenter", mContext,
+                            this, mMessageItem.mSlideshow);
+                } else {
+                    mPresenter.setModel(mMessageItem.mSlideshow);
+                    mPresenter.setView(this);
+                }
+                if (mImageLoadedCallback == null) {
+                    mImageLoadedCallback = new ImageLoadedCallback(this);
+                } else {
+                    mImageLoadedCallback.reset(this);
+                }
+                mPresenter.present(mImageLoadedCallback);
+            }
+        }
+        drawRightStatusIndicator(mMessageItem);
+
+        requestLayout();
+    }
+    
+    private void showMmsView(boolean visible) {
+        if (mMmsView == null) {
+            mMmsView = findViewById(R.id.mms_view);
+            // if mMmsView is still null here, that mean the mms section hasn't been inflated
+
+            if (visible && mMmsView == null) {
+                //inflate the mms view_stub
+                View mmsStub = findViewById(R.id.mms_layout_view_stub);
+                mmsStub.setVisibility(View.VISIBLE);
+                mMmsView = findViewById(R.id.mms_view);
+            }
+        }
+        if (mMmsView != null) {
+            if (mImageView == null) {
+                mImageView = (ImageView) findViewById(R.id.image_view);
+            }
+            if (mSlideShowButton == null) {
+                mSlideShowButton = (ImageButton) findViewById(R.id.play_slideshow_button);
+            }
+            mMmsView.setVisibility(visible ? View.VISIBLE : View.GONE);
+            mImageView.setVisibility(visible ? View.VISIBLE : View.GONE);
+        }
+    }
+    
     ForegroundColorSpan mColorSpan = null;  // set in ctor
+    
+    private CharSequence formatMessage(MessageItem msgItem, String body,
+            String subject, Pattern highlight,
+            String contentType) {
+        SpannableStringBuilder buf = new SpannableStringBuilder();
+
+        boolean hasSubject = !TextUtils.isEmpty(subject);
+        if (hasSubject) {
+            buf.append(mContext.getResources().getString(R.string.inline_subject, subject));
+        }
+
+        if (!TextUtils.isEmpty(body)) {
+            // Converts html to spannable if ContentType is "text/html".
+            if (contentType != null && ContentType.TEXT_HTML.equals(contentType)) {
+                buf.append("\n");
+                buf.append(Html.fromHtml(body));
+            } else {
+                if (hasSubject) {
+                    buf.append(" - ");
+                }
+                buf.append(body);
+            }
+        }
+
+        if (highlight != null) {
+            Matcher m = highlight.matcher(buf.toString());
+            while (m.find()) {
+                buf.setSpan(new StyleSpan(Typeface.BOLD), m.start(), m.end(), 0);
+            }
+        }
+        return buf;
+    }
 }
