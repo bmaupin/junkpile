e9afbfbc4ea8718ec7f68867260a038a02903f0f
bmaupin <bmaupincode@gmail.com>
2014-11-20 14:26:18 -0500

    Clean up a bunch of unused views and add BackgroundQueryHandler

--- java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/ui/ComposeMessageActivity.java
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/ui/ComposeMessageActivity.java
@@ -5,24 +5,33 @@
 
 package ca.bmaupin.merge.sms.ui;
 
+import static com.android.mms.ui.MessageListAdapter.COLUMN_ID;
+
+import java.util.ArrayList;
+
+import com.android.mms.LogTag;
+import com.android.mms.data.Contact;
+import com.android.mms.data.ContactList;
+import com.android.mms.data.Conversation.ConversationQueryHandler;
+import com.android.mms.transaction.MessagingNotification;
+import com.android.mms.ui.ComposeMessageActivity;
+import com.android.mms.ui.ConversationList;
+import com.android.mms.widget.MmsWidgetProvider;
+
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
-import android.text.InputFilter;
-import android.text.InputFilter.LengthFilter;
 import android.util.Log;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.TextView;
 import ca.bmaupin.merge.sms.R;
 import ca.bmaupin.merge.sms.data.Conversation;
 
 public class ComposeMessageActivity extends Activity {
     private ContentResolver mContentResolver;
 
-    private BackgroundQueryHandler mBackgroundQueryHandler;
+//    private BackgroundQueryHandler mBackgroundQueryHandler;
     
     private MessageListView mMsgListView;        // ListView for messages in this conversation
     public MessageListAdapter mMsgListAdapter;  // and its corresponding ListAdapter
@@ -40,7 +49,7 @@ public class ComposeMessageActivity extends Activity {
         initResourceRefs();
 
 // TODO        
-//        mContentResolver = getContentResolver();
+        mContentResolver = getContentResolver();
 //        mBackgroundQueryHandler = new BackgroundQueryHandler(mContentResolver);
 
 // TODO
@@ -62,44 +71,118 @@ public class ComposeMessageActivity extends Activity {
         // input field but when the message list is scrolled that padding area is filled
         // in with message content
         mMsgListView.setClipToPadding(false);
+    }
+    
+    private final class BackgroundQueryHandler extends ConversationQueryHandler {
+        public BackgroundQueryHandler(ContentResolver contentResolver) {
+            super(contentResolver);
+        }
 
-        mMsgListView.setOnSizeChangedListener(new OnSizeChangedListener() {
-            public void onSizeChanged(int width, int height, int oldWidth, int oldHeight) {
-                if (Log.isLoggable(LogTag.APP, Log.VERBOSE)) {
-                    Log.v(TAG, "onSizeChanged: w=" + width + " h=" + height +
-                            " oldw=" + oldWidth + " oldh=" + oldHeight);
-                }
+        @Override
+        protected void onQueryComplete(int token, Object cookie, Cursor cursor) {
+            switch(token) {
+                case MESSAGE_LIST_QUERY_TOKEN:
+                    mConversation.blockMarkAsRead(false);
 
-                if (!mMessagesAndDraftLoaded && (oldHeight-height > SMOOTH_SCROLL_THRESHOLD)) {
-                    // perform the delayed loading now, after keyboard opens
-                    loadMessagesAndDraft(3);
-                }
+                    // check consistency between the query result and 'mConversation'
+                    long tid = (Long) cookie;
 
+                    if (LogTag.VERBOSE || Log.isLoggable(LogTag.APP, Log.VERBOSE)) {
+                        log("##### onQueryComplete: msg history result for threadId " + tid);
+                    }
+                    if (tid != mConversation.getThreadId()) {
+                        log("onQueryComplete: msg history query result is for threadId " +
+                                tid + ", but mConversation has threadId " +
+                                mConversation.getThreadId() + " starting a new query");
+                        if (cursor != null) {
+                            cursor.close();
+                        }
+                        startMsgListQuery();
+                        return;
+                    }
 
-                // The message list view changed size, most likely because the keyboard
-                // appeared or disappeared or the user typed/deleted chars in the message
-                // box causing it to change its height when expanding/collapsing to hold more
-                // lines of text.
-                smoothScrollToEnd(false, height - oldHeight);
+                    // check consistency b/t mConversation & mWorkingMessage.mConversation
+                    ComposeMessageActivity.this.sanityCheckConversation();
+
+                    int newSelectionPos = -1;
+                    long targetMsgId = getIntent().getLongExtra("select_id", -1);
+                    if (targetMsgId != -1) {
+                        if (cursor != null) {
+                            cursor.moveToPosition(-1);
+                            while (cursor.moveToNext()) {
+                                long msgId = cursor.getLong(COLUMN_ID);
+                                if (msgId == targetMsgId) {
+                                    newSelectionPos = cursor.getPosition();
+                                    break;
+                                }
+                            }
+                        }
+                    } else if (mSavedScrollPosition != -1) {
+                        // mSavedScrollPosition is set when this activity pauses. If equals maxint,
+                        // it means the message list was scrolled to the end. Meanwhile, messages
+                        // could have been received. When the activity resumes and we were
+                        // previously scrolled to the end, jump the list so any new messages are
+                        // visible.
+                        if (mSavedScrollPosition == Integer.MAX_VALUE) {
+                            int cnt = mMsgListAdapter.getCount();
+                            if (cnt > 0) {
+                                // Have to wait until the adapter is loaded before jumping to
+                                // the end.
+                                newSelectionPos = cnt - 1;
+                                mSavedScrollPosition = -1;
+                            }
+                        } else {
+                            // remember the saved scroll position before the activity is paused.
+                            // reset it after the message list query is done
+                            newSelectionPos = mSavedScrollPosition;
+                            mSavedScrollPosition = -1;
+                        }
+                    }
+
+                    mMsgListAdapter.changeCursor(cursor);
+
+                    if (newSelectionPos != -1) {
+                        mMsgListView.setSelection(newSelectionPos);     // jump the list to the pos
+                    } else {
+                        int count = mMsgListAdapter.getCount();
+                        long lastMsgId = 0;
+                        if (cursor != null && count > 0) {
+                            cursor.moveToLast();
+                            lastMsgId = cursor.getLong(COLUMN_ID);
+                        }
+                        // mScrollOnSend is set when we send a message. We always want to scroll
+                        // the message list to the end when we send a message, but have to wait
+                        // until the DB has changed. We also want to scroll the list when a
+                        // new message has arrived.
+                        smoothScrollToEnd(mScrollOnSend || lastMsgId != mLastMessageId, 0);
+                        mLastMessageId = lastMsgId;
+                        mScrollOnSend = false;
+                    }
+                    // Adjust the conversation's message count to match reality. The
+                    // conversation's message count is eventually used in
+                    // WorkingMessage.clearConversation to determine whether to delete
+                    // the conversation or not.
+                    mConversation.setMessageCount(mMsgListAdapter.getCount());
+
+                    // Once we have completed the query for the message history, if
+                    // there is nothing in the cursor and we are not composing a new
+                    // message, we must be editing a draft in a new conversation (unless
+                    // mSentMessage is true).
+                    // Show the recipients editor to give the user a chance to add
+                    // more people before the conversation begins.
+                    if (cursor != null && cursor.getCount() == 0
+                            && !isRecipientsEditorVisible() && !mSentMessage) {
+                        initRecipientsEditor();
+                    }
+
+                    // FIXME: freshing layout changes the focused view to an unexpected
+                    // one, set it back to TextEditor forcely.
+                    mTextEditor.requestFocus();
+
+                    invalidateOptionsMenu();    // some menu items depend on the adapter's count
+                    return;
             }
-        });
-
-        mBottomPanel = findViewById(R.id.bottom_panel);
-        mTextEditor = (EditText) findViewById(R.id.embedded_text_editor);
-        mTextEditor.setOnEditorActionListener(this);
-        mTextEditor.addTextChangedListener(mTextEditorWatcher);
-        mTextEditor.setFilters(new InputFilter[] {
-                new LengthFilter(MmsConfig.getMaxTextLimit())});
-        mTextCounter = (TextView) findViewById(R.id.text_counter);
-        mSendButtonMms = (TextView) findViewById(R.id.send_button_mms);
-        mSendButtonSms = (ImageButton) findViewById(R.id.send_button_sms);
-        mSendButtonMms.setOnClickListener(this);
-        mSendButtonSms.setOnClickListener(this);
-        mTopPanel = findViewById(R.id.recipients_subject_linear);
-        mTopPanel.setFocusable(false);
-        mAttachmentEditor = (AttachmentEditor) findViewById(R.id.attachment_editor);
-        mAttachmentEditor.setHandler(mAttachmentEditorHandler);
-        mAttachmentEditorScrollView = findViewById(R.id.attachment_editor_scroll_view);
+        }
     }
     
     public static Intent createIntent(Context context, long threadId) {
