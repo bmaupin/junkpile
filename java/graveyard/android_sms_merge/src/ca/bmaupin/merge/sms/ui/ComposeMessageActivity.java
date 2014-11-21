79355521154ff0465353486c1d5ee2207599ab13
bmaupin <bmaupin@users.noreply.github.com>
2014-11-21 13:58:27 -0500

    Mostly finished adding BackgroundQueryHandler

--- java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/ui/ComposeMessageActivity.java
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/ui/ComposeMessageActivity.java
@@ -5,37 +5,49 @@
 
 package ca.bmaupin.merge.sms.ui;
 
-import static com.android.mms.ui.MessageListAdapter.COLUMN_ID;
-
 import java.util.ArrayList;
-
-import com.android.mms.LogTag;
-import com.android.mms.data.Contact;
-import com.android.mms.data.ContactList;
-import com.android.mms.data.Conversation.ConversationQueryHandler;
-import com.android.mms.transaction.MessagingNotification;
-import com.android.mms.ui.ComposeMessageActivity;
-import com.android.mms.ui.ConversationList;
-import com.android.mms.widget.MmsWidgetProvider;
 
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SqliteWrapper;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import static ca.bmaupin.merge.sms.ui.MessageListAdapter.PROJECTION;
+import ca.bmaupin.merge.sms.LogTag;
 import ca.bmaupin.merge.sms.R;
 import ca.bmaupin.merge.sms.data.Conversation;
+import ca.bmaupin.merge.sms.data.Conversation.ConversationQueryHandler;
 
 public class ComposeMessageActivity extends Activity {
+	private static final String TAG = "Mms/compose";
+	
+    private static final int MESSAGE_LIST_QUERY_TOKEN = 9527;
+	
     private ContentResolver mContentResolver;
 
-//    private BackgroundQueryHandler mBackgroundQueryHandler;
+    private BackgroundQueryHandler mBackgroundQueryHandler;
+
+    private Conversation mConversation;     // Conversation we are working in
     
     private MessageListView mMsgListView;        // ListView for messages in this conversation
     public MessageListAdapter mMsgListAdapter;  // and its corresponding ListAdapter
 	
+    @SuppressWarnings("unused")
+    public static void log(String logMsg) {
+        Thread current = Thread.currentThread();
+        long tid = current.getId();
+        StackTraceElement[] stack = current.getStackTrace();
+        String methodName = stack[3].getMethodName();
+        // Prepend current thread ID and name of calling method to the message.
+        logMsg = "[" + tid + "] [" + methodName + "] " + logMsg;
+        Log.d(TAG, logMsg);
+    }
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 // TODO
@@ -47,10 +59,9 @@ public class ComposeMessageActivity extends Activity {
 
         // Initialize members for UI elements.
         initResourceRefs();
-
-// TODO        
+        
         mContentResolver = getContentResolver();
-//        mBackgroundQueryHandler = new BackgroundQueryHandler(mContentResolver);
+        mBackgroundQueryHandler = new BackgroundQueryHandler(mContentResolver);
 
 // TODO
 //        initialize(savedInstanceState, 0);
@@ -73,6 +84,39 @@ public class ComposeMessageActivity extends Activity {
         mMsgListView.setClipToPadding(false);
     }
     
+    private void startMsgListQuery() {
+        startMsgListQuery(MESSAGE_LIST_QUERY_TOKEN);
+    }
+
+    private void startMsgListQuery(int token) {
+        Uri conversationUri = mConversation.getUri();
+
+        if (conversationUri == null) {
+            log("##### startMsgListQuery: conversationUri is null, bail!");
+            return;
+        }
+
+        long threadId = mConversation.getThreadId();
+        if (LogTag.VERBOSE || Log.isLoggable(LogTag.APP, Log.VERBOSE)) {
+            log("startMsgListQuery for " + conversationUri + ", threadId=" + threadId +
+                    " token: " + token + " mConversation: " + mConversation);
+        }
+
+        // Cancel any pending queries
+        mBackgroundQueryHandler.cancelOperation(token);
+        try {
+            // Kick off the new query
+            mBackgroundQueryHandler.startQuery(
+                    token,
+                    threadId /* cookie */,
+                    conversationUri,
+                    PROJECTION,
+                    null, null, null);
+        } catch (SQLiteException e) {
+            SqliteWrapper.checkSQLiteException(this, e);
+        }
+    }
+    
     private final class BackgroundQueryHandler extends ConversationQueryHandler {
         public BackgroundQueryHandler(ContentResolver contentResolver) {
             super(contentResolver);
@@ -82,8 +126,6 @@ public class ComposeMessageActivity extends Activity {
         protected void onQueryComplete(int token, Object cookie, Cursor cursor) {
             switch(token) {
                 case MESSAGE_LIST_QUERY_TOKEN:
-                    mConversation.blockMarkAsRead(false);
-
                     // check consistency between the query result and 'mConversation'
                     long tid = (Long) cookie;
 
@@ -104,80 +146,13 @@ public class ComposeMessageActivity extends Activity {
                     // check consistency b/t mConversation & mWorkingMessage.mConversation
                     ComposeMessageActivity.this.sanityCheckConversation();
 
-                    int newSelectionPos = -1;
-                    long targetMsgId = getIntent().getLongExtra("select_id", -1);
-                    if (targetMsgId != -1) {
-                        if (cursor != null) {
-                            cursor.moveToPosition(-1);
-                            while (cursor.moveToNext()) {
-                                long msgId = cursor.getLong(COLUMN_ID);
-                                if (msgId == targetMsgId) {
-                                    newSelectionPos = cursor.getPosition();
-                                    break;
-                                }
-                            }
-                        }
-                    } else if (mSavedScrollPosition != -1) {
-                        // mSavedScrollPosition is set when this activity pauses. If equals maxint,
-                        // it means the message list was scrolled to the end. Meanwhile, messages
-                        // could have been received. When the activity resumes and we were
-                        // previously scrolled to the end, jump the list so any new messages are
-                        // visible.
-                        if (mSavedScrollPosition == Integer.MAX_VALUE) {
-                            int cnt = mMsgListAdapter.getCount();
-                            if (cnt > 0) {
-                                // Have to wait until the adapter is loaded before jumping to
-                                // the end.
-                                newSelectionPos = cnt - 1;
-                                mSavedScrollPosition = -1;
-                            }
-                        } else {
-                            // remember the saved scroll position before the activity is paused.
-                            // reset it after the message list query is done
-                            newSelectionPos = mSavedScrollPosition;
-                            mSavedScrollPosition = -1;
-                        }
-                    }
-
                     mMsgListAdapter.changeCursor(cursor);
-
-                    if (newSelectionPos != -1) {
-                        mMsgListView.setSelection(newSelectionPos);     // jump the list to the pos
-                    } else {
-                        int count = mMsgListAdapter.getCount();
-                        long lastMsgId = 0;
-                        if (cursor != null && count > 0) {
-                            cursor.moveToLast();
-                            lastMsgId = cursor.getLong(COLUMN_ID);
-                        }
-                        // mScrollOnSend is set when we send a message. We always want to scroll
-                        // the message list to the end when we send a message, but have to wait
-                        // until the DB has changed. We also want to scroll the list when a
-                        // new message has arrived.
-                        smoothScrollToEnd(mScrollOnSend || lastMsgId != mLastMessageId, 0);
-                        mLastMessageId = lastMsgId;
-                        mScrollOnSend = false;
-                    }
+                    
                     // Adjust the conversation's message count to match reality. The
                     // conversation's message count is eventually used in
                     // WorkingMessage.clearConversation to determine whether to delete
                     // the conversation or not.
                     mConversation.setMessageCount(mMsgListAdapter.getCount());
-
-                    // Once we have completed the query for the message history, if
-                    // there is nothing in the cursor and we are not composing a new
-                    // message, we must be editing a draft in a new conversation (unless
-                    // mSentMessage is true).
-                    // Show the recipients editor to give the user a chance to add
-                    // more people before the conversation begins.
-                    if (cursor != null && cursor.getCount() == 0
-                            && !isRecipientsEditorVisible() && !mSentMessage) {
-                        initRecipientsEditor();
-                    }
-
-                    // FIXME: freshing layout changes the focused view to an unexpected
-                    // one, set it back to TextEditor forcely.
-                    mTextEditor.requestFocus();
 
                     invalidateOptionsMenu();    // some menu items depend on the adapter's count
                     return;
