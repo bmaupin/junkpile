c4743c4846d0ee77f5877bfa805db54c3f67eef5
bmaupin <bmaupincode@gmail.com>
2014-11-21 14:37:33 -0500

    Add parseMmsAddress() for ContactList

--- java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/ui/MessageUtils.java
+++ java/graveyard/android_sms_merge/src/ca/bmaupin/merge/sms/ui/MessageUtils.java
@@ -5,13 +5,32 @@
 
 package ca.bmaupin.merge.sms.ui;
 
+import java.util.HashMap;
+
 import android.content.Context;
+import android.provider.Telephony.Mms;
 import android.text.format.DateUtils;
 import android.text.format.Time;
 
 import ca.bmaupin.merge.sms.MmsConfig;
 
 public class MessageUtils {
+    /**
+     * MMS address parsing data structures
+     */
+    // allowable phone number separators
+    private static final char[] NUMERIC_CHARS_SUGAR = {
+        '-', '.', ',', '(', ')', ' ', '/', '\\', '*', '#', '+'
+    };
+
+    private static HashMap numericSugarMap = new HashMap (NUMERIC_CHARS_SUGAR.length);
+
+    static {
+        for (int i = 0; i < NUMERIC_CHARS_SUGAR.length; i++) {
+            numericSugarMap.put(NUMERIC_CHARS_SUGAR[i], NUMERIC_CHARS_SUGAR[i]);
+        }
+    }
+	
     public static String formatTimeStampString(Context context, long when) {
         return formatTimeStampString(context, when, false);
     }
@@ -73,5 +92,62 @@ public class MessageUtils {
         }
 
         return true;
+    }
+    
+    /**
+     * Given a phone number, return the string without syntactic sugar, meaning parens,
+     * spaces, slashes, dots, dashes, etc. If the input string contains non-numeric
+     * non-punctuation characters, return null.
+     */
+    private static String parsePhoneNumberForMms(String address) {
+        StringBuilder builder = new StringBuilder();
+        int len = address.length();
+
+        for (int i = 0; i < len; i++) {
+            char c = address.charAt(i);
+
+            // accept the first '+' in the address
+            if (c == '+' && builder.length() == 0) {
+                builder.append(c);
+                continue;
+            }
+
+            if (Character.isDigit(c)) {
+                builder.append(c);
+                continue;
+            }
+
+            if (numericSugarMap.get(c) == null) {
+                return null;
+            }
+        }
+        return builder.toString();
+    }
+    
+    /**
+     * parse the input address to be a valid MMS address.
+     * - if the address is an email address, leave it as is.
+     * - if the address can be parsed into a valid MMS phone number, return the parsed number.
+     * - if the address is a compliant alias address, leave it as is.
+     */
+    public static String parseMmsAddress(String address) {
+        // if it's a valid Email address, use that.
+        if (Mms.isEmailAddress(address)) {
+            return address;
+        }
+
+        // if we are able to parse the address to a MMS compliant phone number, take that.
+        String retVal = parsePhoneNumberForMms(address);
+        if (retVal != null) {
+            return retVal;
+        }
+
+        // if it's an alias compliant address, use that.
+        if (isAlias(address)) {
+            return address;
+        }
+
+        // it's not a valid MMS address, return null
+        return null;
     }
 }
