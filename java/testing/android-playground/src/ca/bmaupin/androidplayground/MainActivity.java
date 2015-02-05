b071ad1e6417fd57d578a439466fc5e6a0b65232
bmaupin <bmaupincode@gmail.com>
2015-02-05 12:32:30 -0500

    Get toggle for enabling data working

--- java/testing/android-playground/src/ca/bmaupin/androidplayground/MainActivity.java
+++ java/testing/android-playground/src/ca/bmaupin/androidplayground/MainActivity.java
@@ -1,9 +1,16 @@
 package ca.bmaupin.androidplayground;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ToggleButton;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -30,5 +37,37 @@ public class MainActivity extends ActionBarActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	public void onToggleClicked(View view) {
+	    // Is the toggle on?
+	    boolean on = ((ToggleButton) view).isChecked();
+	    
+	    if (on) {
+	        ConnectivityManager dataManager;
+	        dataManager  = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+	        Method dataMtd = null;
+	        try {
+	            dataMtd = ConnectivityManager.class.getDeclaredMethod("setMobileDataEnabled", boolean.class);
+	        } catch (NoSuchMethodException e) {
+	            // TODO Auto-generated catch block
+	            e.printStackTrace();
+	        }
+	        dataMtd.setAccessible(true);
+	        try {
+	            dataMtd.invoke(dataManager, true);
+	        } catch (IllegalArgumentException e) {
+	            // TODO Auto-generated catch block
+	            e.printStackTrace();
+	        } catch (IllegalAccessException e) {
+	            // TODO Auto-generated catch block
+	            e.printStackTrace();
+	        } catch (InvocationTargetException e) {
+	            // TODO Auto-generated catch block
+	            e.printStackTrace();
+	        }
+	    } else {
+	        // Disable data
+	    }
 	}
 }
