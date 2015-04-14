66efa794843e21cb08e994c8136a089625f365d6
bmaupin <bmaupin@users.noreply.github.com>
2015-04-14 14:17:34 -0400

    Add package definition so we can run go fmt, and run go fmt to standardize formatting

--- go/misc/functions.go
+++ go/misc/functions.go
@@ -1,136 +1,138 @@
 // Misc functions
 
+package functions
+
 func copyFile(sourceFilePath string, destFilePath string) error {
-    r, err := os.Open(sourceFilePath)
-    if err != nil {
-        return err
-    }
-    defer func() {
-        if err := r.Close(); err != nil {
-            log.Fatalf("os.File.Close error: %s", err)
-        }
-    }()
-    
-    w, err := os.Create(destFilePath)
-    if err != nil {
-        return err
-    }
-    defer func() {
-        if err := w.Close(); err != nil {
-            log.Fatalf("os.File.Close error: %s", err)
-        }
-    }()
-    
-    _, err = io.Copy(w, r)
-    if err != nil {
-        return err
-    }
-    
-    return nil
+	r, err := os.Open(sourceFilePath)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := r.Close(); err != nil {
+			log.Fatalf("os.File.Close error: %s", err)
+		}
+	}()
+
+	w, err := os.Create(destFilePath)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := w.Close(); err != nil {
+			log.Fatalf("os.File.Close error: %s", err)
+		}
+	}()
+
+	_, err = io.Copy(w, r)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func zipFile(sourceFilePath string, destFilePath string) error {
-    r, err := os.Open(sourceFilePath)
-    if err != nil {
-        return err
-    }
-    defer func() {
-        if err := r.Close(); err != nil {
-            log.Fatalf("os.File.Close error: %s", err)
-        }
-    }()
-    
-    f, err := os.Create(destFilePath)
-    if err != nil {
-        return err
-    }
-    defer func() {
-        if err := f.Close(); err != nil {
-            log.Fatalf("os.File.Close error: %s", err)
-        }
-    }()
-    
-    z := zip.NewWriter(f)
-    defer func() {
-        if err := z.Close(); err != nil {
-            log.Fatalf("zip.Writer.Close error: %s", err)
-        }
-    }()
-    
-    w, err := z.Create(sourceFilePath)
-    if err != nil {
-        return err
-    }
-    
-    _, err = io.Copy(w, r)
-    if err != nil {
-        return err
-    }
-    
-    return nil
+	r, err := os.Open(sourceFilePath)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := r.Close(); err != nil {
+			log.Fatalf("os.File.Close error: %s", err)
+		}
+	}()
+
+	f, err := os.Create(destFilePath)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Fatalf("os.File.Close error: %s", err)
+		}
+	}()
+
+	z := zip.NewWriter(f)
+	defer func() {
+		if err := z.Close(); err != nil {
+			log.Fatalf("zip.Writer.Close error: %s", err)
+		}
+	}()
+
+	w, err := z.Create(sourceFilePath)
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(w, r)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func zipFolder(sourceFolderPath string, destFilePath string) error {
-    f, err := os.Create(destFilePath)
-    if err != nil {
-        return err
-    }
-    defer func() {
-        if err := f.Close(); err != nil {
-            log.Fatalf("os.File.Close error: %s", err)
-        }
-    }()
-    
-    z := zip.NewWriter(f)
-    defer func() {
-        if err := z.Close(); err != nil {
-            log.Fatalf("zip.Writer.Close error: %s", err)
-        }
-    }()
-    
-    var addFileToZip = func(path string, info os.FileInfo, err error) error {
-        if err != nil {
-            return err
-        }
-        
-        // Get the path of the file relative to the folder we're zipping
-        relativePath, err := filepath.Rel(sourceFolderPath, path)
-        if err != nil {
-            return err
-        }
-        
-        // Only include regular files, not directories
-        if !info.Mode().IsRegular() {
-            return nil
-        }
-        
-        r, err := os.Open(path)
-        if err != nil {
-            return err
-        }
-        defer func() {
-            if err := r.Close(); err != nil {
-                log.Fatalf("os.File.Close error: %s", err)
-            }
-        }()
-        
-        w, err := z.Create(relativePath)
-        if err != nil {
-            return err
-        }
-        
-        _, err = io.Copy(w, r)
-        if err != nil {
-            return err
-        }
-        
-        return nil
-    }
-    
-    err = filepath.Walk(sourceFolderPath, addFileToZip)
-    if err != nil {
-        return err
-    }
-    
-    return nil
+	f, err := os.Create(destFilePath)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Fatalf("os.File.Close error: %s", err)
+		}
+	}()
+
+	z := zip.NewWriter(f)
+	defer func() {
+		if err := z.Close(); err != nil {
+			log.Fatalf("zip.Writer.Close error: %s", err)
+		}
+	}()
+
+	var addFileToZip = func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Get the path of the file relative to the folder we're zipping
+		relativePath, err := filepath.Rel(sourceFolderPath, path)
+		if err != nil {
+			return err
+		}
+
+		// Only include regular files, not directories
+		if !info.Mode().IsRegular() {
+			return nil
+		}
+
+		r, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer func() {
+			if err := r.Close(); err != nil {
+				log.Fatalf("os.File.Close error: %s", err)
+			}
+		}()
+
+		w, err := z.Create(relativePath)
+		if err != nil {
+			return err
+		}
+
+		_, err = io.Copy(w, r)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+
+	err = filepath.Walk(sourceFolderPath, addFileToZip)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
