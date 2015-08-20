193da1f67f556e39de062cb196caa8b20b520107
bmaupin <bmaupin@users.noreply.github.com>
2015-08-20 15:26:00 -0400

    Minor cleanup

--- groovy/grails-test/grails-app/controllers/langpop/CountController.groovy
+++ groovy/grails-test/grails-app/controllers/langpop/CountController.groovy
@@ -33,51 +33,7 @@ class CountController {
         }
 */
 //        }
-            /*
-            def notLangs = [
-                'AppleScript',
-                'ASP',
-                'Arduino',
-                'Batchfile',
-                'CMake',
-                'CSS',
-                'Cuda',
-                'HTML',
-                'HTTP',
-                'JSON',
-                'Makefile',
-                'Max',
-                'Nginx',
-                'Processing',
-                'Puppet',
-                'QML',
-                'SQL',
-                'TeX',
-                'VimL',
-                'XML',
-                'XSLT',
-            ]
 
-            def langCounts = [:]
-            Lang.list().each { lang ->
-                if (!(lang.name in notLangs)) {
-                    def langTotalCount = 0
-
-                    Site.list().each { site ->
-                        def query = Count.where {
-                            lang.id == lang.id && site.id == site.id && date == new Date().clearTime()
-                        }
-                        if (query.count() == 1) {
-                            langTotalCount += query.get().count
-                        }
-                    }
-
-                    langCounts[lang] = langTotalCount
-                }
-            }
-
-            render langCounts
-            */
 /*
         render {
             "<h4>Top languages overall:</h4>"
