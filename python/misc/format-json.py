c0f0164612b2cfe1e3728c81956fc93ec1a50101
bmaupin <bmaupin@users.noreply.github.com>
2016-01-07 13:56:35 -0500

    Add quick script for formatting a flat json file

---
+++ python/misc/format-json.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import json
+import sys
+
+
+def main():
+    if len(sys.argv) < 3:
+        print('Usage: {} INFILE OUTFILE'.format(sys.argv[0]))
+        sys.exit()
+    
+    with open(sys.argv[1], 'r') as infile, open(sys.argv[2], 'w') as outfile:
+        outfile.write(
+            json.dumps(
+                json.loads(
+                    infile.read()
+                ),
+                sort_keys=True,
+                indent=4
+            )
+        )
+
+
+if __name__ == '__main__':
+    main()
