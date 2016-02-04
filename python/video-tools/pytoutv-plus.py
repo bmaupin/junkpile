c55d4f9bd8a171acf9079b06f8ea57f2008c6a26
bmaupin <bmaupin@users.noreply.github.com>
2016-02-04 15:07:47 -0500

    Get list working again

--- python/video-tools/pytoutv-plus.py
+++ python/video-tools/pytoutv-plus.py
@@ -149,6 +149,7 @@ class CliError(RuntimeError):
 
 
 class AppPlus(toutvcli.app.App):
+    # TODO: put data in its own class
     DATA_DOWNLOADED = 'downloaded'
     DATA_EMISSIONS = 'emissions'
     DATA_FIRST_SEEN = 'first_seen'
@@ -239,11 +240,9 @@ class AppPlus(toutvcli.app.App):
     def _print_list_emissions(self, all=False):
         def title_sort_func(ekey):
             return locale.strxfrm(repertoire_emissions[ekey].get_title())
-        
-        client = get_client_with_cache()
 
         # Get the list of current emissions
-        repertoire = retry_function(client.get_page_repertoire)
+        repertoire = retry_function(self._toutvclient.get_page_repertoire)
         repertoire_emissions = retry_function(repertoire.get_emissions)
 
         # For some reason the emission ID is an int, but JSON converts int keys to
@@ -251,43 +250,43 @@ class AppPlus(toutvcli.app.App):
         repertoire_emissions = {str(k):v for k, v in repertoire_emissions.items()}
 
         today = datetime.datetime.now().strftime('%Y-%m-%d')
-        data = get_data()
+        data = self.get_data()
 
         # If this is the first run ever or the first run of the day
-        if DATA_LAST_RUN not in data or data[DATA_LAST_RUN] != today:
+        if self.DATA_LAST_RUN not in data or data[self.DATA_LAST_RUN] != today:
             # If this is the first run ever
-            if DATA_LAST_RUN not in data:
-                data[DATA_LAST_RUN] = today
-                data[DATA_EMISSIONS] = {}
+            if self.DATA_LAST_RUN not in data:
+                data[self.DATA_LAST_RUN] = today
+                data[self.DATA_EMISSIONS] = {}
             
             # Start with a fresh list of new emissions
-            data[DATA_NEW_EMISSIONS] = []
+            data[self.DATA_NEW_EMISSIONS] = []
             
             for emission_id in repertoire_emissions:
                 # If this is a completely new emission
-                if emission_id not in data[DATA_EMISSIONS]:
-                    data[DATA_NEW_EMISSIONS].append(emission_id)
+                if emission_id not in data[self.DATA_EMISSIONS]:
+                    data[self.DATA_NEW_EMISSIONS].append(emission_id)
                     
-                    data[DATA_EMISSIONS][emission_id] = {}
-                    data[DATA_EMISSIONS][emission_id][DATA_TITLE] = repertoire_emissions[emission_id].Title
-                    data[DATA_EMISSIONS][emission_id][DATA_FIRST_SEEN] = today
-                    data[DATA_EMISSIONS][emission_id][DATA_NEW_COUNT] = 1
+                    data[self.DATA_EMISSIONS][emission_id] = {}
+                    data[self.DATA_EMISSIONS][emission_id][self.DATA_TITLE] = repertoire_emissions[emission_id].Title
+                    data[self.DATA_EMISSIONS][emission_id][self.DATA_FIRST_SEEN] = today
+                    data[self.DATA_EMISSIONS][emission_id][self.DATA_NEW_COUNT] = 1
                 
                 # If this is a new emission since the last run
-                if DATA_LAST_SEEN in data[DATA_EMISSIONS][emission_id] and \
-                        data[DATA_EMISSIONS][emission_id][DATA_LAST_SEEN] != \
-                        data[DATA_LAST_RUN]:
-                    data[DATA_EMISSIONS][emission_id][DATA_NEW_COUNT] += 1
+                if self.DATA_LAST_SEEN in data[self.DATA_EMISSIONS][emission_id] and \
+                        data[self.DATA_EMISSIONS][emission_id][self.DATA_LAST_SEEN] != \
+                        data[self.DATA_LAST_RUN]:
+                    data[self.DATA_EMISSIONS][emission_id][self.DATA_NEW_COUNT] += 1
                     
-                    if data[DATA_EMISSIONS][emission_id][DATA_NEW_COUNT] <= \
+                    if data[self.DATA_EMISSIONS][emission_id][self.DATA_NEW_COUNT] <= \
                             MAX_NEW_COUNT:
-                        data[DATA_NEW_EMISSIONS].append(emission_id)
+                        data[self.DATA_NEW_EMISSIONS].append(emission_id)
                 
-                data[DATA_EMISSIONS][emission_id][DATA_LAST_SEEN] = today
+                data[self.DATA_EMISSIONS][emission_id][self.DATA_LAST_SEEN] = today
                 
                 # Basic sanity check if title of an emission has changed
                 if repertoire_emissions[emission_id].Title.lower() != \
-                        data[DATA_EMISSIONS][emission_id][DATA_TITLE].lower():
+                        data[self.DATA_EMISSIONS][emission_id][self.DATA_TITLE].lower():
                     sys.stderr.write(
                         'Warning: title mismatch\n'
                         '\tId: {}\n'
@@ -295,34 +294,33 @@ class AppPlus(toutvcli.app.App):
                         '\tData file title: {}\n'.format(
                             emission_id,
                             repertoire_emissions[emission_id].Title,
-                            data[DATA_EMISSIONS][emission_id][DATA_TITLE]
+                            data[self.DATA_EMISSIONS][emission_id][self.DATA_TITLE]
                             )
                         )
             
             # Sort the list of new emissions alphabetically
-            data[DATA_NEW_EMISSIONS].sort(key=title_sort_func)
+            data[self.DATA_NEW_EMISSIONS].sort(key=title_sort_func)
 
-            data[DATA_LAST_RUN] = today
+            data[self.DATA_LAST_RUN] = today
             
-            write_data(data)
+            self.write_data(data)
         
         # List all emissions
-        if args.all:
+        if all:
             emissions_keys = list(repertoire_emissions.keys())
             emissions_keys.sort(key=title_sort_func)
             
-            list_emissions(repertoire_emissions, emissions_keys)
+            self.list_emissions(repertoire_emissions, emissions_keys)
             
         # List only new emissions
         else:
-            if len(data[DATA_NEW_EMISSIONS]) == 0:
+            if len(data[self.DATA_NEW_EMISSIONS]) == 0:
                 print('No new emissions since last run')
-                print('To list all emissions, see: {} {} --help\n'.format(
-                    sys.argv[0],
-                    args.command))
+                print('To list all emissions, see: {} list --help\n'.format(
+                    sys.argv[0]))
                 
             else:
-                list_emissions(repertoire_emissions, data[DATA_NEW_EMISSIONS])
+                self.list_emissions(repertoire_emissions, data[self.DATA_NEW_EMISSIONS])
     
     def _fetch_episode(self, episode, output_dir, bitrate, quality, overwrite):
         # Get available bitrates for episode
@@ -351,7 +349,7 @@ class AppPlus(toutvcli.app.App):
         # Finished
         self._dl = None
 
-    def get_data_file_path():
+    def get_data_file_path(self):
         DATA_FILE_NAME = 'toutv_data.json'
         
         if 'XDG_DATA_HOME' in os.environ:
@@ -369,22 +367,22 @@ class AppPlus(toutvcli.app.App):
             
         return data_path
 
-    def get_data():
-        data_path = get_data_file_path()
+    def get_data(self):
+        data_path = self.get_data_file_path()
         if not os.path.exists(data_path):
             return {}
         else:
             with open(data_path, 'r') as data_file:
                 return json.loads(data_file.read())
 
-    def write_data(data):
-        data_path = get_data_file_path()
+    def write_data(self, data):
+        data_path = self.get_data_file_path()
         with open(data_path, 'w') as data_file:
             data_file.write(
                 json.dumps(data, sort_keys=True, indent=4, ensure_ascii=False)
             )
 
-    def command_fetch(args):
+    def command_fetch(self, args):
         client = get_client_with_cache()
         if args.emission is not None:
             emission = retry_function(client.get_emission_by_name, args.emission)
@@ -393,7 +391,7 @@ class AppPlus(toutvcli.app.App):
         app = AppPlus(None)
         app._verbose = False
         
-        data = get_data()
+        data = self.get_data()
         
         # Download single episode
         if args.episode is not None:
@@ -404,16 +402,15 @@ class AppPlus(toutvcli.app.App):
         else:
             episodes = retry_function(client.get_emission_episodes, emission)
             for episode_id in eposides:
-                if (emission.Id in data[DATA_EMISSIONS] and
-                        DATA_DOWNLOADED in data[DATA_EMISSIONS][emission.Id]):
+                if (emission.Id in data[self.DATA_EMISSIONS] and
+                        self.DATA_DOWNLOADED in data[self.DATA_EMISSIONS][emission.Id]):
                     # TODO: do the comparison
                     sys.exit('Error: not yet implemented')
                 
                 else:
                     sys.exit('Error: not yet implemented')
 
-
-    def list_emissions(all_emissions, emissions_to_list):
+    def list_emissions(self, all_emissions, emissions_to_list):
         for emission_id in emissions_to_list:
             emission = all_emissions[emission_id]
             emission_string = ('{}\n\t{}\n\t{}'.format(
