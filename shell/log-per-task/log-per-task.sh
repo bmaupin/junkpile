ec3873aa407b08138435fd684ea912bf987672cd
bmaupin <bmaupin@users.noreply.github.com>
2016-02-09 12:48:09 -0500

    Lowercase and don't export variable to avoid conflicts

--- shell/log-per-task/log-per-task.sh
+++ shell/log-per-task/log-per-task.sh
@@ -1,4 +1,4 @@
-export LOG_FILE=log.txt
+log_file=log.txt
 
 
 # Script that logs per task
@@ -18,7 +18,7 @@ export LOG_FILE=log.txt
 # Log success/failure for each task
 begin() {
     if [ "$next_task" == "" ]; then
-        stdbuf -o0 echo "`basename \"$0\"`:" | tee -a $LOG_FILE
+        stdbuf -o0 echo "`basename \"$0\"`:" | tee -a $log_file
         error_count=0
         next_task=$@
         return
@@ -26,9 +26,9 @@ begin() {
     task=$next_task
     next_task=$@
     if [ "$error_count" -eq 0 ]; then
-        stdbuf -o0 echo "  [X] ${task}" | tee -a $LOG_FILE
+        stdbuf -o0 echo "  [X] ${task}" | tee -a $log_file
     else
-        stdbuf -o0 echo "  [ ] ERROR completing task: ${task}" | tee -a $LOG_FILE
+        stdbuf -o0 echo "  [ ] ERROR completing task: ${task}" | tee -a $log_file
     fi
     error_count=0
 }
@@ -41,7 +41,7 @@ trap begin EXIT
 # Log specific commands that return errors
 log_failure() {
     this_command="$(history 1 | sed -e "s/^[ ]*[0-9]*[ ]*//g")";
-    stdbuf -o0 echo "      ERROR running command: ${this_command}" | tee -a $LOG_FILE
+    stdbuf -o0 echo "      ERROR running command: ${this_command}" | tee -a $log_file
     error_count=$((error_count+1))
 }
 set -o history
@@ -49,4 +49,4 @@ trap log_failure ERR
 
 
 # Send STDERR to log file
-exec 2>> $LOG_FILE
+exec 2>> $log_file
