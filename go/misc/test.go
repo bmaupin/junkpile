9d40a43ccdb586406e195b779594ec5a8fed6010
bmaupin <bmaupin@users.noreply.github.com>
2016-06-09 15:38:44 -0400

    Add function for printing out HTML node info

--- go/misc/test.go
+++ go/misc/test.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+
+	"github.com/bmaupin/go-util/htmlutil"
+	"golang.org/x/net/html"
 )
 
 func main() {
@@ -15,4 +18,25 @@ func main() {
 			u := uuid.NewV4()
 			fmt.Println(u)
 	*/
+}
+
+func debugNode(node *html.Node) {
+	fmt.Printf("type: %s\n", node.Type)
+	if node.Type == html.CommentNode {
+		fmt.Println("type: CommentNode")
+	} else if node.Type == html.DoctypeNode {
+		fmt.Println("type: DoctypeNode")
+	} else if node.Type == html.DocumentNode {
+		fmt.Println("type: DocumentNode")
+	} else if node.Type == html.ElementNode {
+		fmt.Println("type: ElementNode")
+	} else if node.Type == html.ErrorNode {
+		fmt.Println("type: ErrorNode")
+	} else if node.Type == html.TextNode {
+		fmt.Println("type: TextNode")
+	}
+
+	fmt.Printf("data: %s\n", node.Data)
+	fmt.Printf("attr: %s\n", node.Attr)
+	fmt.Println(htmlutil.HtmlNodeToString(node))
 }
