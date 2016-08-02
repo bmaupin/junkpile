26cdab318f9404f5812cb390ba2cdd31d9b908b8
bmaupin <bmaupin@users.noreply.github.com>
2016-08-02 14:46:00 -0400

    Only process dates before what's already in the database

--- groovy/grails-test/scripts/ImportSOCounts.groovy
+++ groovy/grails-test/scripts/ImportSOCounts.groovy
@@ -21,16 +21,26 @@ def inputStream = new ByteArrayInputStream(XmlExamples.CAR_RECORDS.getBytes("UTF
 reader.parse(new InputSource(inputStream))
 */
 
+import groovy.transform.Field
 import javax.xml.parsers.SAXParser
 import javax.xml.parsers.SAXParserFactory
 import org.xml.sax.helpers.DefaultHandler
 import org.xml.sax.*
 
 class MyHandler extends DefaultHandler {
+    def lastDate = Date.parse('yyyy-MM-dd', '2015-07-15')
+
     void startElement(String ns, String localName, String qName, Attributes atts) {
         switch (qName) {
-           case 'row':
-               println atts.getValue('CreationDate'); break
+            case 'row':
+                // Parse the date, dropping the time
+                def creationDate = Date.parse('yyyy-MM-dd', atts.getValue('CreationDate'))
+
+                if (creationDate <= lastDate) {
+                    println creationDate
+                }
+
+                break
         }
     }
     /*
@@ -52,6 +62,6 @@ class MyHandler extends DefaultHandler {
 
 SAXParserFactory factory = SAXParserFactory.newInstance()
 SAXParser parser = factory.newSAXParser()
-File file = new File('/home/bmaupin/Desktop/temp-so/Posts.xml')
+File file = new File('/home/bmaupin/Desktop/temp-so/Posts-head.xml')
 DefaultHandler handler = new MyHandler()
 parser.parse(file, handler)
