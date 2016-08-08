e2363fefdcd1e36199c0baa2aedc55415c0d5740
bmaupin <bmaupin@users.noreply.github.com>
2016-08-08 14:04:36 -0400

    Add function for getting github language name

--- groovy/grails-test/src/groovy/langpop/ImportUtil.groovy
+++ groovy/grails-test/src/groovy/langpop/ImportUtil.groovy
@@ -18,4 +18,22 @@ public class ImportUtil {
     static List<String> getStackoverflowLangNames() {
         return Lang.list().collect { getStackoverflowLangName(it) }
     }
+
+    static String getGitHubLangName(String name) {
+        def a = LangAltName.find {
+            altName == name && site.id == Site.findByName(STACKOVERFLOW_SITE_NAME).id
+        }
+
+        if (a != null) {
+            return Lang.findById(a.lang.id).name
+
+        } else {
+            def gitHubLangName = Lang.findByNameIlike(name)?.name
+            if (gitHubLangName != null) {
+                return gitHubLangName
+            } else {
+                return Lang.findByNameIlike(name.replaceAll('-', ' '))?.name
+            }
+        }
+    }
 }
