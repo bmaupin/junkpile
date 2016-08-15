e550467dfaa03bf3a5b2c3962160d46dc07cf5d6
bmaupin <bmaupin@users.noreply.github.com>
2016-08-15 14:53:51 -0400

    Go ahead and total the github counts while we import the data

--- groovy/grails-test/scripts/ImportGithubHistory.groovy
+++ groovy/grails-test/scripts/ImportGithubHistory.groovy
@@ -1,11 +1,13 @@
 import langpop.*
 
 
-def OLDEST_GITHUB_DATE = '2007-10-29'
-
-def searchDate = Date.parse('yyyy-MM-dd', OLDEST_GITHUB_DATE)
+def FIRST_DATE = ImportUtil.GITHUB_OLDEST_DATE
 // This is the last date of data that isn't in the DB already
-def lastDate = Date.parse('yyyy-MM-dd', '2015-07-15')
+def LAST_DATE = '2015-07-15'
+
+def firstDate = Date.parse('yyyy-MM-dd', FIRST_DATE)
+def searchDate = firstDate
+def lastDate = Date.parse('yyyy-MM-dd', LAST_DATE)
 
 def ghSite = Site.findByName(ImportUtil.GITHUB_SITE_NAME)
 
@@ -17,18 +19,35 @@ langNames.each{ langName ->
     langs.add(Lang.findByName(langName))
 }
 
+// This will hold the running total count for each language
+def totalCounts = [:]
+
 while (searchDate <= lastDate) {
     langs.each{ lang ->
         // Be verbose in case we have to abort the script
         println "${searchDate}\t${lang.name}"
 
-        def count = ImportUtil.getGithubRepoCount(lang.name, searchDate)
+        // Populate the total counts from the DB if this is the first date
+        if (searchDate == firstDate) {
+            def query = Count.where {
+                date == searchDate - 1 && lang.id == lang.id && site.id == ghSite.id
+            }
+            def count = query.find()
 
-        if (count != 0) {
+            if (count == null) {
+                count = 0
+            }
+
+            totalCounts[lang.name] = count
+        }
+
+        totalCounts[lang.name] += ImportUtil.getGithubRepoCount(lang.name, searchDate)
+
+        if (totalCounts[lang.name] != 0) {
             new Count(
                 // Remove the time component of the date just to be safe
                 date: searchDate.clearTime(),
-                count: count,
+                count: totalCounts[lang.name],
                 lang: lang,
                 site: ghSite
             // TODO: this save doesn't get persisted without flush: true...
