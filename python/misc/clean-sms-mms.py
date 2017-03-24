f41b3df92198bc46ba3a890cbf7f43777692e63b
bmaupin <bmaupin@users.noreply.github.com>
2017-03-24 14:05:11 -0400

    Fix bug when removing elements while iterating through them

--- python/misc/clean-sms-mms.py
+++ python/misc/clean-sms-mms.py
@@ -22,12 +22,18 @@ shutil.copy2(infile_name, outfile_name)
 # Remove any SMS/MMS messages older than MAXIMUM_MESSAGE_AGE_IN_DAYS
 root = lxml.etree.parse(infile_name)
 
+elements_to_remove = []
+
 for element in root.iter():
     if element.tag == '{http://www.titaniumtrack.com/ns/titanium-backup/messages}sms' \
         or element.tag == '{http://www.titaniumtrack.com/ns/titanium-backup/messages}mms':
             message_date = datetime.datetime.strptime(element.get('date'), '%Y-%m-%dT%H:%M:%S.%fZ')
             if datetime.datetime.now() - message_date > datetime.timedelta(MAXIMUM_MESSAGE_AGE_IN_DAYS):
-                element.getparent().remove(element)
+                # We can't remove elements while we're iterating through them
+                elements_to_remove.append(element)
+
+for element in elements_to_remove:
+    element.getparent().remove(element)
 
 with open(infile_name, 'wb') as infile:
     infile.write(lxml.etree.tostring(root, pretty_print=True, xml_declaration=True))
