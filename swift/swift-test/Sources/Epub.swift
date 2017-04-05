4a7a8d2399c1b2ea1d4c0b52db32f709962cc4a9
bmaupin <bmaupin@users.noreply.github.com>
2017-04-05 14:26:19 -0400

    Add init(fromFile: )

--- swift/swift-test/Sources/Epub.swift
+++ swift/swift-test/Sources/Epub.swift
@@ -1,7 +1,36 @@
-public struct Epub {
-    var title: String
+import Foundation
+import PerfectZip
 
-    init(title: String) {
-        self.title = title
+public struct Epub {
+    let tempDirectoryPrefix = "swift-epub"
+
+    var tempDirectory: URL!
+
+    private init() {
+        tempDirectory = Util.createTempDirectory(prefix: tempDirectoryPrefix)
+    }
+
+    init(fromFile fileURL: URL) {
+        self.init()
+        // TODO handle this error better
+        do {
+            try extractEpubFile(fileURL: fileURL)
+        } catch (let error) {
+            print(error)
+        }
+    }
+
+    func extractEpubFile(fileURL: URL) throws {
+        let zip = Zip()
+
+        let UnzipResult = zip.unzipFile(
+            source: fileURL.path,
+            destination: tempDirectory.path,
+            overwrite: true
+        )
+
+        if UnzipResult != ZipStatus.ZipSuccess {
+            throw UnzipResult
+        }
     }
 }
