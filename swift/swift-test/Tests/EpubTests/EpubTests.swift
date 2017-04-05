06348bd9c8572fa086081fb3a6c88fb479c2dff6
bmaupin <bmaupin@users.noreply.github.com>
2017-04-05 13:47:54 -0400

    Write test for Epub(fromFile:)

--- swift/swift-test/Tests/EpubTests/EpubTests.swift
+++ swift/swift-test/Tests/EpubTests/EpubTests.swift
@@ -3,18 +3,25 @@ import XCTest
 
 class EpubTests: XCTestCase {
     var epub: Epub!
-    var epubTitle = "My title"
+    // var epubTitle = "My title"
 
     override func setUp() {
         super.setUp()
 
-        epub = Epub(title: epubTitle)
+        // epub = Epub(title: epubTitle)
     }
 
     override func tearDown() {
         // TODO: add code to delete generated epub
 
         super.tearDown()
+    }
+
+    func testEpubInitFromFile() {
+        // TODO: Bundle.init(for: ) not yet implemented in Linux
+        //let testEpubFilePath = Bundle(for: type(of: self)).url(forResource: "test", withExtension: "epub")!
+        let testEpubFilePath = Bundle(path: FileManager.default.currentDirectoryPath + "/Tests/EpubTests")!.url(forResource: "test", withExtension: "epub")!
+        let _ = Epub(fromFile: testEpubFilePath)
     }
 
     // func testExample() {
@@ -25,7 +32,7 @@ class EpubTests: XCTestCase {
 
     static var allTests : [(String, (EpubTests) -> () throws -> Void)] {
         return [
-            // ("testExample", testExample),
+            ("testEpubInitFromFile", testEpubInitFromFile),
         ]
     }
 }
