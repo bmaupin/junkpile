ad965d7a3209a06c081369188f80bb40f0101420
bmaupin <bmaupin@users.noreply.github.com>
2017-04-05 13:47:18 -0400

    Add PerfectZip as a dependency

--- swift/swift-test/Package.swift
+++ swift/swift-test/Package.swift
@@ -1,5 +1,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "Epub"
+    name: "Epub",
+    dependencies: [
+        // TODO: Replace this with a pure Swift zip library when that becomes available because:
+        //  - A pure Swift library wouldn't require separate installation of C zip libraries
+        //  - Pure Swift libraries can be used within the REPL
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Zip.git",
+                 majorVersion: 2)
+    ]
 )
