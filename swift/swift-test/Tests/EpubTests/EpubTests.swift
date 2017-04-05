1331320e323c2c3c54e87d60209ccb11a3b8ce4d
bmaupin <bmaupin@users.noreply.github.com>
2017-04-05 14:14:00 -0400

    Clean up tests

--- swift/swift-test/Tests/EpubTests/EpubTests.swift
+++ swift/swift-test/Tests/EpubTests/EpubTests.swift
@@ -3,7 +3,6 @@ import XCTest
 
 class EpubTests: XCTestCase {
     var epub: Epub!
-    // var epubTitle = "My title"
     var testEpubFilePath: URL!
 
     override func setUp() {
@@ -13,25 +12,19 @@ class EpubTests: XCTestCase {
         //let bundle = Bundle(for: type(of: self))
         let bundle = Bundle(path: FileManager.default.currentDirectoryPath + "/Tests/EpubTests")!
         testEpubFilePath = bundle.url(forResource: "test", withExtension: "epub")!
-
-        // epub = Epub(title: epubTitle)
     }
 
     override func tearDown() {
-        // TODO: add code to delete generated epub
+        // TODO: add cleanup code
 
         super.tearDown()
     }
 
     func testEpubInitFromFile() {
         let _ = Epub(fromFile: testEpubFilePath)
-    }
 
-    // func testExample() {
-    //     // This is an example of a functional test case.
-    //     // Use XCTAssert and related functions to verify your tests produce the correct results.
-    //     XCTAssertEqual(Epub().text, "Hello, World!")
-    // }
+        // TODO: test to make sure the epub's actually been successfully opened (get title, etc)
+    }
 
     static var allTests : [(String, (EpubTests) -> () throws -> Void)] {
         return [
