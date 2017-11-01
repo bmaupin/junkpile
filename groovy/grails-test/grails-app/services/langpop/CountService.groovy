8a82682d76725eb91f94fdffbf24ef391a992a27
bmaupin <bmaupin@users.noreply.github.com>
2017-11-01 12:08:03 -0400

    Add another batch of non-languages

--- groovy/grails-test/grails-app/services/langpop/CountService.groovy
+++ groovy/grails-test/grails-app/services/langpop/CountService.groovy
@@ -14,6 +14,7 @@ class CountService {
      * - Metalanguages (ABNF, EBNF, etc)
      * - File types (Adobe Font Metrics, C-ObjDump, etc)
      * - Templating formats (Closure Templates, EJS, Java Server Pages, etc)
+     * - Esoteric programming languages (Brainfuck, Dogescript, LOLCODE, etc)
      */
     def nonLangs = [
         'ABNF',
@@ -34,10 +35,12 @@ class CountService {
         'AutoHotkey',
         'AutoIt',
         'Batchfile',
+        'Befunge',
         'Bison',
         'BitBake',
         'Blade',
         'Bluespec',
+        'Brainfuck',
         'Brightscript',
         'Bro',
         'C-ObjDump',
@@ -80,7 +83,7 @@ class CountService {
         'Easybuild',
         'EBNF',
         'Ecere Projects',
-        // 'ECL', // ??
+        'ECL', // https://github.com/hpcc-systems/ecl-ml ??
         'ECLiPSe', // http://eclipseclp.org/
         'edn',
         'EJS',
@@ -108,17 +111,138 @@ class CountService {
         'Glyph', // Pointwise's scripting language, .glf
         'GN', // Generate Ninja, https://chromium.googlesource.com/chromium/src/tools/gn/+/HEAD/docs/language.md, .gn
         'Gnuplot',
+        'Gradle',
+        'Grammatical Framework',
+        'Graph Modeling Language',
+        'GraphQL',
+        'Graphviz (DOT)',
+        'Groovy Server Pages',
+        'Haml',
+        'Handlebars',
+        'HCL', // https://github.com/hashicorp/hcl
+        'HLSL', // https://en.wikipedia.org/wiki/High-Level_Shading_Language
         'HTML',
-        'HTTP',
+        'HTML+Django',
+        'HTML+ECR',
+        'HTML+EEX',
+        'HTML+ERB',
+        'HTML+PHP',
+        'HTTP', // httpspec??
+        'HyPhy', // https://en.wikipedia.org/wiki/HYPHY_(software)
+        'IGOR Pro',
+        'Inform 7',
+        'INI',
+        'Inno Setup',
+        'IRC log',
+        'Isabelle', // https://en.wikipedia.org/wiki/Isabelle_(proof_assistant)
+        'Isabelle ROOT', // Isabelle Session ROOT specification file (https://isabelle.in.tum.de/doc/system.pdf)
+        'Jasmin', // https://en.wikipedia.org/wiki/Jasmin_(software)
         'Java Server Pages',
+        'JFlex',
+        'Jison',
+        'Jison Lex',
         'JSON',
+        'JSON5',
+        'JSONiq',
+        'JSONLD',
+        'JSX',
+        'Jupyter Notebook',
+        'KiCad Layout',
+        'KiCad Legacy Layout',
+        'KiCad Schematic',
+        'Kit', // https://codekitapp.com/help/kit/
+        'KRL', // https://en.wikipedia.org/wiki/KUKA_Robot_Language
+        'LabVIEW',
+        'Latte', // https://latte.nette.org/en/ ??
+        'Lean', // https://leanprover.github.io/
+        'Less',
+        'Lex',
+        'LilyPond',
+        'Linker Script',
+        'Linux Kernel Module',
+        'Liquid', // https://shopify.github.io/liquid/
+        'Literate Agda',
+        'Literate CoffeeScript',
+        'Literate Haskell',
+        'LLVM',
+        'Logos', // http://iphonedevwiki.net/index.php/Logos
+        'LOLCODE',
+        'LookML',
+        'LoomScript',
+        'LSL', // https://en.wikipedia.org/wiki/Linden_Scripting_Language
+        'M4',
+        'M4Sugar',
+        'Mako', // http://www.makotemplates.org/
         'Makefile',
         'Markdown',
-        'Max',
+        'Marko', // https://markojs.com/
+        'Mask', // https://github.com/atmajs/MaskJS
+        'Maven POM',
+        'Max', // https://en.wikipedia.org/wiki/Max_(software)
+        'MAXScript', // MAXScript is the built-in scripting language for Autodesk ® 3ds Max ® and Autodesk ® 3ds Max ® Design. (http://docs.autodesk.com/3DSMAX/14/ENU/MAXScript%20Help%202012/)
+        'MediaWiki',
+        'Meson', // http://mesonbuild.com
+        'Metal', // This is just C with the Metal API (https://developer.apple.com/metal/)
+        'Modelica', // Modeling language, not programming language
+        'Module Management System', // HPE Module Management System for OpenVMS
+        'Monkey', // For video games
+        'Moocode', // https://en.wikipedia.org/wiki/MOO
+        'MQL4',
+        'MQL5',
+        'MTML', // Movable Type Markup Language
+        'MUF', // Multi-User Forth (https://en.wikipedia.org/wiki/MUF_(programming_language))
+        'mupad', // https://en.wikipedia.org/wiki/MuPAD
+        'Myghty', // https://pythonhosted.org/Myghty/
+        'NCL', // https://www.ncl.ucar.edu/overview.shtml
+        'Nearley', // https://nearley.js.org/
+        'nesC',
+        'NetLinx',
+        'NetLinx+ERB',
+        'NetLogo',
         'Nginx',
+        'Ninja', // https://ninja-build.org/
+        'Nix', // https://nixos.org/
+        'NL', // https://en.wikipedia.org/wiki/Nl_(format)
+        'NSIS', // https://en.wikipedia.org/wiki/Nullsoft_Scriptable_Install_System
+        'Nu',
+        'NumPy',
         'ObjDump',
+        'Objective-J', // Pretty much exclusive to Cappucino (http://www.cappuccino-project.org/learn/objective-j.html)
+        'Omgrofl',
+        'OpenCL',
+        'OpenEdge ABL',
+        'OpenRC runscript',
+        'OpenSCAD',
+        'OpenType Feature File',
+        'Org', // https://en.wikipedia.org/wiki/Org-mode
+        'Ox',
+        'P4',
+        'Pan',
+        'Papyrus', // https://www.creationkit.com/fallout4/index.php?title=Papyrus_Introduction
+        'Parrot',
+        'Parrot Assembly',
+        'Parrot Internal Representation',
+        'PAWN', // https://www.compuphase.com/pawn/
+        'Pep8', // Pep/8 assembly language
+        'Perl 6',
+        'Pic', // https://en.wikipedia.org/wiki/Pic_language
+        'Pickle', // Python Pickle data dump
+        'PigLatin', // https://en.wikipedia.org/wiki/Pig_(programming_tool)
+        'PLpgSQL',
+        'PLSQL',
+        'Pod', // https://en.wikipedia.org/wiki/Plain_Old_Documentation
+        'PostScript',
+        'POV-Ray SDL',
+        'PowerBuilder', // AKA PowerScript (https://en.wikipedia.org/wiki/PowerBuilder)
         'Processing',
+        'Propeller Spin', // https://en.wikipedia.org/wiki/Parallax_Propeller
+        'Protocol Buffer',
+        'Public Key',
+        'Pug', // https://github.com/pugjs/pug
         'Puppet',
+        'Pure Data', // https://en.wikipedia.org/wiki/Pure_Data
+        'Python console',
+        'Python traceback',
         'QML',
         'RenderScript',
         'SQL',
