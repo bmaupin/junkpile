ec775abe37ee6d7965e7a30ff36accec5a8dc73c
bmaupin <bmaupin@users.noreply.github.com>
2017-12-19 11:11:55 -0500

    Add function to test network connectivity

--- python/misc/functions.py
+++ python/misc/functions.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python
 
+import socket
+
 def convert_filetime_to_epoch(filetime):
     return (filetime / 10000000) - 11644473600
+
+# Can be used to test connectivity if telnet isn't installed (https://stackoverflow.com/a/33117579/399105)
+def test_connectivity(host, port, timeout=3):
+    try:
+        socket.setdefaulttimeout(timeout)
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+        return True
+    except Exception as ex:
+        print(ex.message)
+        return False
