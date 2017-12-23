018f230e4865635c28d89c5b48165907e6cfc581
bmaupin <bmaupin@users.noreply.github.com>
2017-12-23 13:35:35 -0500

    Upgrade to xcode 9.2 (swift 4) and PerfectZip 3

--- swift/swift-test/Package.swift
+++ swift/swift-test/Package.swift
@@ -4,9 +4,8 @@ let package = Package(
     name: "Epub",
     dependencies: [
         // TODO: Replace this with a pure Swift zip library when that becomes available because:
-        //  - A pure Swift library wouldn't require separate installation of C zip libraries
         //  - Pure Swift libraries can be used within the REPL
         .Package(url: "https://github.com/PerfectlySoft/Perfect-Zip.git",
-                 majorVersion: 2)
+                 majorVersion: 3)
     ]
 )
