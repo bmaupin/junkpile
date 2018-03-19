628d1048debc9613277383c9678b82434310e4d8
Bryan Maupin <bmaupin@users.noreply.github.com>
2018-03-19 11:20:19 -0400

    Unpin everything from Start menu

--- powershell/configure-windows-10.ps1
+++ powershell/configure-windows-10.ps1
@@ -9,12 +9,12 @@
 # Source: https://gallery.technet.microsoft.com/scriptcenter/How-to-disable-Cortana-on-b44924a4
 # License: TechNet terms of use (https://gallery.technet.microsoft.com/scriptcenter/site/How-to-disable-Cortana-on-b44924a4/eulapartial?licenseType=TechNet)
 function DisableCortana {
-    $path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"    
-    if (!(Test-Path -Path $path)) { 
+    $path = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search"
+    if (!(Test-Path -Path $path)) {
         New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows" -Name "Windows Search"
-    } 
-    Set-ItemProperty -Path $path -Name "AllowCortana" -Value 0 
-    # Restart Explorer to change it immediately    
+    }
+    Set-ItemProperty -Path $path -Name "AllowCortana" -Value 0
+    # Restart Explorer to change it immediately
     Stop-Process -name explorer
 }
 DisableCortana
@@ -26,7 +26,7 @@ function ShowFileExtensions {
     Set-Location HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced
     Set-ItemProperty . HideFileExt "0"
     Pop-Location
-    # Restart Explorer to change it immediately    
+    # Restart Explorer to change it immediately
     Stop-Process -name explorer
 }
 ShowFileExtensions
@@ -34,11 +34,34 @@ ShowFileExtensions
 # Source: https://stackoverflow.com/a/45152368/399105
 # License: cc by-sa 3.0 with attribution
 function UnpinFromTaskbar($appname) {
-    ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | 
-    ?{$_.Name -eq $appname}).Verbs() | 
-    ?{$_.Name.replace('&','') -match 'Unpin from taskbar'} | 
+    ((New-Object -Com Shell.Application).NameSpace("shell:::{4234d49b-0245-4df3-b780-3893943456e1}").Items() |
+    ?{$_.Name -eq $appname}).Verbs() |
+    ?{$_.Name.replace("&", "") -match "Unpin from taskbar"} |
     %{$_.DoIt(); $exec = $true}
 }
 UnpinFromTaskbar("Mail")
 UnpinFromTaskbar("Microsoft Edge")
 UnpinFromTaskbar("Microsoft Store")
+
+# Source: https://superuser.com/q/1191143/93066
+# License: cc by-sa 3.0 with attribution
+function UnpinFromStart($appname) {
+    ((New-Object -Com Shell.Application).NameSpace("shell:::{4234d49b-0245-4df3-b780-3893943456e1}").Items() |
+    ?{$_.Name -eq $appname}).Verbs() |
+    ?{$_.Name.replace("&", "") -match "Unpin from Start"} |
+    %{$_.DoIt(); $exec = $true}
+}
+UnpinFromStart("Calendar")
+UnpinFromStart("Groove Music")
+UnpinFromStart("Mail")
+UnpinFromStart("Maps")
+UnpinFromStart("Microsoft Edge")
+UnpinFromStart("Microsoft Store")
+UnpinFromStart("Movies & TV")
+UnpinFromStart("My Office")
+UnpinFromStart("OneNote")
+UnpinFromStart("Paint 3D")
+UnpinFromStart("Photos")
+UnpinFromStart("Skype")
+UnpinFromStart("Weather")
+UnpinFromStart("Xbox")
