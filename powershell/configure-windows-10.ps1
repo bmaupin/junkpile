ccfdbb3520dd70177b7132660c8ec4d8e8df9db0
Bryan Maupin <bmaupin@users.noreply.github.com>
2018-03-19 10:34:29 -0400

    Don't hide file extensions

--- powershell/configure-windows-10.ps1
+++ powershell/configure-windows-10.ps1
@@ -6,7 +6,6 @@
 # 3. Run the file
 # ./configure-windows-10.ps1
 
-# Disable Cortana
 # Source: https://gallery.technet.microsoft.com/scriptcenter/How-to-disable-Cortana-on-b44924a4
 # License: TechNet terms of use (https://gallery.technet.microsoft.com/scriptcenter/site/How-to-disable-Cortana-on-b44924a4/eulapartial?licenseType=TechNet)
 Function DisableCortana {
@@ -19,3 +18,15 @@ Function DisableCortana {
     Stop-Process -name explorer
 }
 DisableCortana
+
+# Source: https://superuser.com/a/896408/93066
+# License: cc by-sa 3.0 with attribution
+function ShowFileExtensions {
+    Push-Location
+    Set-Location HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced
+    Set-ItemProperty . HideFileExt "0"
+    Pop-Location
+    # Restart Explorer to change it immediately    
+    Stop-Process -name explorer
+}
+ShowFileExtensions
