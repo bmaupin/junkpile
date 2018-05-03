162309fb98b9b5b5ddc122cee0761cdc5dcfbf7e
bmaupin <bmaupin@users.noreply.github.com>
2018-05-03 12:12:17 -0400

    Add simple script to add random numerical prefix to files

---
+++ javascript/misc/prefix-files.js
@@ -0,0 +1,51 @@
+/*
+ * This will add a numerical prefix (e.g. "005 - ") to all of the files in the current folder in random order
+ */
+
+const fs = require('fs');
+
+const PREFIX_LENGTH = 3;
+
+function main() {
+  let filenames = getFilesInCurrentDirectory();
+  let shuffledFilenames = shuffleArray(filenames);
+  let prefixLength = getPrefixLength(filenames.length);
+
+  for (let i = 0; i < filenames.length; i++) {
+    let oldFilename = filenames[i];
+    let prefix = String(shuffledFilenames.indexOf(oldFilename) + 1).padStart(prefixLength, '0');
+    let newFilename = `${prefix} - ${oldFilename}`;
+
+    console.log(`Renaming ${oldFilename} to ${newFilename}`);
+
+    renameFile(oldFilename, newFilename);
+  }
+}
+
+function getFilesInCurrentDirectory() {
+  return fs.readdirSync('.');
+}
+
+// https://gist.github.com/guilhermepontes/17ae0cc71fa2b13ea8c20c94c5c35dc4
+function shuffleArray(arr) {
+  return arr
+    .map(a => [Math.random(), a])
+    .sort((a, b) => a[0] - b[0])
+    .map(a => a[1]);
+}
+
+function getPrefixLength(numFilenames) {
+  if (typeof PREFIX_LENGTH !== 'undefined' && PREFIX_LENGTH !== null) {
+    return PREFIX_LENGTH;
+  } else {
+    return String(numFilenames + 1).length;
+  }
+}
+
+function renameFile(oldFilename, newFilename) {
+  fs.rename(oldFilename, newFilename, (err) => {
+    if (err) throw err;
+  });
+}
+
+main();
