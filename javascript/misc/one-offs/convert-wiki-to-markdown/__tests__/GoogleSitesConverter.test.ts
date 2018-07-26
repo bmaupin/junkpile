3ca926eefd6e8e86f08523c6af39492cbf12694a
bmaupin <bmaupin@users.noreply.github.com>
2018-07-26 13:28:09 -0400

    Add test with code in a list

--- javascript/misc/one-offs/convert-wiki-to-markdown/__tests__/GoogleSitesConverter.test.ts
+++ javascript/misc/one-offs/convert-wiki-to-markdown/__tests__/GoogleSitesConverter.test.ts
@@ -2,7 +2,13 @@ import fs from 'fs';
 import GoogleSitesConverter from '../GoogleSitesConverter';
 
 test('Convert page with code', async () => {
-  let markdown = fs.readFileSync('./__tests__/resources/clamav.md').toString('utf8');
-  let converted = await GoogleSitesConverter.convertFromFile('./__tests__/resources/clamav.html');
-  expect(converted).toBe(markdown);
+  let markdownFromFile = fs.readFileSync('./__tests__/resources/clamav.md').toString('utf8');
+  let convertedMarkdown = await GoogleSitesConverter.convertFromFile('./__tests__/resources/clamav.html');
+  expect(convertedMarkdown).toBe(markdownFromFile);
+});
+
+test('Convert page with code in a list', async () => {
+  let markdownFromFile = fs.readFileSync('./__tests__/resources/cups.md').toString('utf8');
+  let convertedMarkdown = await GoogleSitesConverter.convertFromFile('./__tests__/resources/cups.html');
+  expect(convertedMarkdown).toBe(markdownFromFile);
 });
