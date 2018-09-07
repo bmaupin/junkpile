3a29a2915407a846690acec3c313daf7fe7197d3
bmaupin <bmaupin@users.noreply.github.com>
2018-09-07 13:38:46 -0400

    Fix typescript errors, fix more tests, add a couple more tests

--- javascript/misc/one-offs/convert-wiki-to-markdown/convert-wiki-to-markdown.ts
+++ javascript/misc/one-offs/convert-wiki-to-markdown/convert-wiki-to-markdown.ts
@@ -8,8 +8,8 @@ import GoogleSitesConverter from './GoogleSitesConverter';
 const WIKI_URL = 'https://sites.google.com/site/bmaupinwiki/home/programming/tools/meld';
 
 async function main(): Promise<void> {
-  let markdown = await GoogleSitesConverter.convertFromUrl(WIKI_URL);
-  // let markdown = await GoogleSitesConverter.convertFromFile('./__tests__/resources/clamav.html');
+  let markdown = await GoogleSitesConverter.convertPageFromUrl(WIKI_URL);
+  // let markdown = await GoogleSitesConverter.convertPageFromFile('./__tests__/resources/clamav.html');
   console.log(markdown);
 }
 
